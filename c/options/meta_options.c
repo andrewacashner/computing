0c3ac1e2ce389f7f6c334fa69c796f950b08cc38
Andrew A. Cashner <andrewacashner@gmail.com>
2016-11-28 15:28:10 -0800

    Working read_options with global variables modified by options

---
+++ c/options/meta_options.c
@@ -0,0 +1,12 @@
+
+process_options( { "help", NULL, OPT_HELP },
+		 { "version", NULL, OPT_VERSION },
+		 { "frequency", int frequency, OPT_FREQUENCY },
+		 { "rate", int rate, OPT_RATE },
+		 { NULL, char filename[], MAIN_ARG } );
+struct {
+     char option_text[MAX_STRING];
+     
+void process_options(struct option_profile[]){
+     return;
+}
--- c/options/read_options.c
+++ c/options/read_options.c
@@ -23,25 +23,49 @@
 
 /* Function prototypes */
 void process_options(int argc, char *argv[]);
-void read_short_option(char option_input, char parameter[]);
-void read_long_option(char option_input[], char parameter[]);
+void read_option(int option_type, char option_input[], char parameter[]);
 void read_main_arg(char arg_input[]);
-void do_option(int option_code, char parameter[]);
+void do_option(int option_code, char option_input[], char parameter[]);
+
+
+/* Constants for lookup */
+const enum { SHORT_OPTION, LONG_OPTION } option_type;
+
+const char *option_string[] = {
+     "help", "version", "frequency", "rate"
+};
+const enum {
+     OPT_HELP,
+     OPT_VERSION,
+     OPT_FREQUENCY,
+     OPT_RATE,
+     OPT_MISSING_PARAMETER,
+     OPT_UNKNOWN
+} option_code;
+
+/* Error messages */
+const char *typeout_msg[] = {
+     "Help info",
+     "Version number",
+     "Bad option",
+     "Bad parameter",
+     "Missing parameter for option",
+     "Unknown error"
+};
+const enum {
+     MSG_HELP,
+     MSG_VERSION,
+     MSG_BAD_OPTION,
+     MSG_BAD_PARAMETER,
+     MSG_MISSING_PARAMETER,
+     MSG_UNKNOWN_ERROR
+} typeout_msg_code;
+
 
 /* Global variables */
 int global_frequency = 1000;
 int global_rate = 20;
 char global_filename[80];
-
-/* Constants for lookup */
-const char *option_string[] = {
-     "help", "version", "frequency", "rate"
-};
-const enum {
-     OPT_HELP, OPT_VERSION, OPT_FREQUENCY, OPT_RATE,
-     OPT_UNKNOWN
-} option_code;
-
 
 int main(int argc, char *argv[])
 {
@@ -50,7 +74,7 @@ int main(int argc, char *argv[])
      printf("Frequency = %d\n"
 	    "Rate      = %d\n"
 	    "Filename  = '%s'\n",
-	    frequency, rate, filename);
+	    global_frequency, global_rate, global_filename);
 
      return(0);
 }
@@ -62,30 +86,31 @@ void process_options(int argc, char *argv[])
      for (i = 1; i < argc; ++i) {
 	  if (argv[i][0] == '-') {
 	       if (argv[i][1] == '-') {
-		    read_long_option(&argv[i][2], argv[i + 1]);
+		    read_option( (int)LONG_OPTION, &argv[i][2], argv[i + 1]);
 		    ++i;
 	       } else if (argv[i][2] == '\0') {
-		    read_short_option(argv[i][1], argv[i + 1]);
+		    read_option( (int)SHORT_OPTION, &argv[i][1], argv[i + 1]);
 		    ++i;
-	       } else {
-		    do_option(OPT_UNKNOWN, "");
-	       }
-	  } else {
-	       read_main_arg(argv[i]);
+	       } else do_option(OPT_UNKNOWN, argv[1], NULL);
+	  } else read_main_arg(argv[i]);
+     }
+     return;
+}
+
+
+void read_option(int option_type, char option_input[], char parameter[])
+{
+     int i;
+     for (i = 0; i < OPT_UNKNOWN; ++i) {
+	  if (option_input[0] == option_string[i][0]) {
+	       if (option_type == LONG_OPTION) {
+		    if (strcmp(option_input, option_string[i]) == 0) {
+			 break;
+		    }
+	       } else break;
 	  }
      }
-
-     return;
-}
-
-
-void read_short_option(char option_input, char parameter[])
-{
-     return;
-}
-
-void read_long_option(char option_input[], char parameter[])
-{
+     do_option(i, option_input, parameter);
      return;
 }
 
@@ -95,8 +120,39 @@ void read_main_arg(char arg_input[])
      return;
 }
 
-void do_option(int option_code, char parameter[])
+void do_option(int option_code, char option_input[], char parameter[])
 {
+     switch (option_code) {
+     case OPT_HELP:
+	  printf("%s\n", typeout_msg[MSG_HELP]);
+	  exit(0);
+     case OPT_VERSION:
+	  printf("%s\n", typeout_msg[MSG_VERSION]);
+	  exit(0);
+     case OPT_FREQUENCY:
+	  if (parameter == NULL) {
+	       do_option(OPT_MISSING_PARAMETER, option_input, parameter);
+	  } else if (sscanf(parameter, "%d", &global_frequency) != 1) {
+	       fprintf(stderr, "%s '%s'\n", typeout_msg[MSG_BAD_PARAMETER], parameter);
+	       exit(EXIT_FAILURE);
+	  } else break;
+     case OPT_RATE:
+	  if (parameter == NULL) {
+	       do_option(OPT_MISSING_PARAMETER, option_input, parameter);
+	  } else if (sscanf(parameter, "%d", &global_rate) != 1) {
+	       fprintf(stderr, "%s '%s'\n", typeout_msg[MSG_BAD_PARAMETER], parameter);
+	       exit(EXIT_FAILURE);
+	  } else break;
+     case OPT_UNKNOWN:
+	  fprintf(stderr, "%s '%s'\n", typeout_msg[MSG_BAD_OPTION], option_input);
+	  exit(EXIT_FAILURE);
+     case OPT_MISSING_PARAMETER:
+	  fprintf(stderr, "%s '%s'\n", typeout_msg[MSG_MISSING_PARAMETER], option_input);
+	  exit(EXIT_FAILURE);
+     default:
+	  fprintf(stderr, "%s\n", typeout_msg[MSG_UNKNOWN_ERROR]);
+	  exit(EXIT_FAILURE);
+     }
      
      return;
 }
