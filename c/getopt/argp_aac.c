5148671d7d956cf618e9c2b430e12ca9df088e62
Andrew A. Cashner <andrewacashner@gmail.com>
2016-12-02 08:51:28 -0800

    working argp test

--- c/getopt/argp_aac.c
+++ c/getopt/argp_aac.c
@@ -2,23 +2,25 @@
 
 #include <stdlib.h>
 #include <argp.h>
+#include <stdbool.h>
 
 const char *argp_program_version =
      "argp test 1.0";
 const char *argp_program_bug_address =
      "<andrewacashner@gmail.com>";
-static char *doc =
+static char doc[] =
      "argp test -- command-line options and arguments with argp";
-static char *args_doc = "FILE";
-
+static char args_doc[] =
+     "INFILE OUTFILE";
 static struct argp_option options[] = {
-     {"output", 'o', "FILE", 0, "Output to FILE instead of standard output" },
+     {"format", 'f', "FORMAT", 0, "Specify output format (default: .txt)" },
      { 0 }
 };
 
 struct arguments {
-     char *args;
-     char *output_file;
+     char *args[2];
+     bool format_mode;
+     char *format_string;
 };
 
 static error_t parse_opt(int key, char *arg, struct argp_state *state)
@@ -26,14 +28,15 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state)
      struct arguments *arguments = state->input;
 
      switch (key) {
-     case 'o':
-	  arguments->output_file = arg;
+     case 'f':
+	  arguments->format_string = arg;
+	  arguments->format_mode = true;
 	  break;
      case ARGP_KEY_ARG:
-	  if (state->arg_num >= 1) {
+	  if (state->arg_num >= 2) {
 	       argp_usage(state);
 	  }
-	  arguments->args = arg;
+	  arguments->args[state->arg_num] = arg;
 	  break;
      case ARGP_KEY_END:
 	  if (state->arg_num < 2) {
@@ -50,13 +53,20 @@ static struct argp argp = { options, parse_opt, args_doc, doc };
 int main(int argc, char *argv[])
 {
      struct arguments arguments;
-     arguments.output_file = "-";
-
+     char *infile_name, *outfile_name;
+     arguments.format_string = "txt";
+     
      argp_parse(&argp, argc, argv, 0, 0, &arguments);
 
-     printf("Input file (arg1) = %s\n"
+     infile_name = arguments.args[0];
+     outfile_name = arguments.args[1];
+
+     if (arguments.format_mode == true) {
+	  printf("Using file format %s...\n", arguments.format_string);
+     }
+     printf("Input file  = %s\n"
 	    "Output file = %s\n",
-	    arguments.args[0], arguments.output_file);
+	    infile_name, outfile_name);
 
      return(0);
 }
