c605472c5fd11237888727f415037617a8b2fd8a
Andrew A. Cashner <andrewacashner@gmail.com>
2016-12-04 14:31:24 -0800

    barely working roman numeral converter

---
+++ c/roman/roman.c
@@ -0,0 +1,63 @@
+/* roman.c -- Convert arabic to roman numerals
+ * Andrew Cashner, 2016/12/04
+ * Initial version, additive only (9 = VIIII, not IX)
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_STRING 80
+
+const char roman_char[] = "IVXLCDM";
+const int roman_int[] = {1, 5, 10, 50, 100, 500, 1000};
+const int max_roman_chars = 7;
+
+int to_roman(int arabic, char *roman);
+
+int main(int argc, char *argv[])
+{
+     int arabic;
+     char roman[MAX_STRING];
+     
+     if (argc != 2) {
+	  fprintf(stderr, "Usage: roman INTEGER\n");
+	  exit(EXIT_FAILURE);
+     }
+     if (sscanf(argv[1], "%d", &arabic) != 1) {
+	  fprintf(stderr, "Bad argument %s.\n", argv[1]);
+	  exit(EXIT_FAILURE);
+     }
+     
+     if (to_roman(arabic, roman) == 0) {
+	  printf("%s\n", roman);
+     } else {
+	  fprintf(stderr, "Input number out of range.\n");
+	  exit(EXIT_FAILURE);
+     }
+	  
+     return(0);
+}
+
+int to_roman(int arabic, char *roman)
+{
+     int i;
+     int roman_index = 0;
+
+     if (arabic <= 0 || arabic >= 1000*1000) {
+	  return(1);
+     }
+     while (arabic > 0) {
+	  for (i = max_roman_chars; i > 0; --i) {
+	       if (arabic >= roman_int[i]) {
+		    break;
+	       }
+	  }
+	  roman[roman_index] = roman_char[i];
+	  ++roman_index;
+	  arabic -= roman_int[i];
+     }
+
+     roman[roman_index] = '\0';
+     return(0);
+}
