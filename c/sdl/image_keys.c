d81f2b7e5b3dc4d9bc91b1d0e103ac368f197969
Andrew A. Cashner <andrewacashner@gmail.com>
2016-12-24 11:48:49 -0600

    Error messages and basic SDL picture-display code separated into functions

--- c/sdl/image_keys.c
+++ c/sdl/image_keys.c
@@ -10,6 +10,28 @@
 
 #define MAX_STRING 80
 
+/* Lookup tables */
+const char *error_msg[] = {
+  "Couldn't initialize SDL",
+  "Couldn't create window and renderer",
+  "Couldn't create surface from image",
+  "Couldn't create texture from surface",
+  "Unknown error\n"
+};
+const enum {
+  INITIALIZE_ERROR,
+  WINDOW_RENDERER_ERROR,
+  SURFACE_ERROR,
+  TEXTURE_ERROR,
+  UNKNOWN_ERROR
+} error_types;
+
+/* Function prototypes */
+void exit_SDL_error(int error_code);
+void display_picture(char *image_filename, SDL_Renderer *renderer,
+		     SDL_Surface *surface, SDL_Texture *texture);
+
+
 int main(void)
 {
   SDL_Window *window;
@@ -25,15 +47,11 @@ int main(void)
   bool show_picture = false;
 
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-		 "Couldn't initialize SDL: %s", SDL_GetError());
-    return(3);
+    exit_SDL_error(INITIALIZE_ERROR);
   }
   if (SDL_CreateWindowAndRenderer(640, 580, SDL_WINDOW_RESIZABLE,
 				  &window, &renderer)) {
-    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-		 "Couldn't create window and renderer: %s", SDL_GetError());
-    return(3);
+    exit_SDL_error(WINDOW_RENDERER_ERROR);
   }
 
   while (quit == false) {
@@ -73,30 +91,10 @@ int main(void)
       SDL_RenderClear(renderer);
 
       if (show_picture == true) {
-	surface = SDL_LoadBMP(image_filename);
-	if (!surface) {
-	  SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-		       "Couldn't create surface from image: %s", SDL_GetError());
-	  return(3);
-	}
-    
-	texture = SDL_CreateTextureFromSurface(renderer, surface);
-	if (!texture) {
-	  SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-		       "Couldn't create texture from surface: %s", SDL_GetError());
-	  return(3);
-	}
-	SDL_FreeSurface(surface);
-	
-	SDL_RenderCopy(renderer, texture, NULL, NULL);
-	SDL_RenderPresent(renderer);
-	SDL_Delay(1000);
-	SDL_DestroyTexture(texture);
+	display_picture(image_filename, renderer, surface, texture);
 	show_picture = false;
-	
       } else {
-	SDL_RenderCopy(renderer, NULL, NULL, NULL);
-	SDL_RenderPresent(renderer);
+	display_picture(NULL, renderer, surface, texture);
       }
     }
   }
@@ -108,3 +106,43 @@ int main(void)
 
   return(0);
 }
+
+
+void exit_SDL_error(int error_code)
+{
+  if (error_code >= UNKNOWN_ERROR) {
+    fprintf(stderr, "%s", error_msg[UNKNOWN_ERROR]);
+  } else {
+    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+		 "%s: %s", error_msg[error_code], SDL_GetError());
+  }
+  exit(EXIT_FAILURE);
+}
+  
+void display_picture(char *image_filename, SDL_Renderer *renderer,
+		     SDL_Surface *surface, SDL_Texture *texture)
+{
+  if (image_filename == NULL) {
+        SDL_RenderCopy(renderer, NULL, NULL, NULL);
+	SDL_RenderPresent(renderer);
+  } else {
+    surface = SDL_LoadBMP(image_filename);
+    if (!surface) {
+      exit_SDL_error(SURFACE_ERROR);
+    }
+    
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
+    if (!texture) {
+      exit_SDL_error(TEXTURE_ERROR);
+    }
+    SDL_FreeSurface(surface);
+    
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderPresent(renderer);
+    SDL_Delay(1000);
+    SDL_DestroyTexture(texture);
+  }
+  return;
+}
+
+  
