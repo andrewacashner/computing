6ec10fe3f7090f4b3f6bd0d61dce16e6fbc5a0dc
Andrew A. Cashner <andrewacashner@gmail.com>
2017-01-04 13:05:50 -0600

    Command line option to set directory added to image_keys

--- c/sdl/cats.c
+++ c/sdl/cats.c
@@ -5,103 +5,105 @@
 
 int main(void)
 {
-  SDL_Window *window;
-  SDL_Renderer *renderer;
-  SDL_Surface *surface;
-  SDL_Texture *texture;
-  SDL_Event event;
-  bool quit = false;
-  bool cats = false;
-  int i;
-  int rgb_rainbow[6][3] = { {255, 0, 0},
-			    {255, 127, 0},
-			    {255, 255, 0},
-			    {0, 255, 0},
-			    {0, 0, 255},
-			    {75, 0, 130} };
-      
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-		 "Couldn't initialize SDL: %s", SDL_GetError());
-    return(3);
-  }
-  if (SDL_CreateWindowAndRenderer(640,640,
-				  SDL_WINDOW_RESIZABLE, &window, &renderer)) {
-    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-		 "Couldn't create window and renderer: %s", SDL_GetError());
-    return(3);
-  }
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Surface *surface;
+    SDL_Texture *texture;
+    SDL_Event event;
+    bool quit = false;
+    bool cats = false;
+    int i;
+    int rgb_rainbow[6][3] = { 
+        {255, 0, 0},
+        {255, 127, 0},
+        {255, 255, 0},
+        {0, 255, 0},
+        {0, 0, 255},
+        {75, 0, 130} 
+    };
 
-  surface = SDL_LoadBMP("img/cat.bmp");
-  if (!surface) {
-    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-		 "Couldn't create surface from image: %s", SDL_GetError());
-    return(3);
-  }
-  texture = SDL_CreateTextureFromSurface(renderer, surface);
-  if (!texture) {
-    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-		 "Couldn't create texture from surface: %s", SDL_GetError());
-  }
-  SDL_FreeSurface(surface);
-
-  while (quit == false) {
-    for (i = 0; i < 6; ++i ) {
-      while (SDL_PollEvent(&event)) {
-	switch (event.type) {
-	case SDL_QUIT:
-	  quit = true;
-	  break;
-	case SDL_WINDOWEVENT:
-	  switch (event.window.event) {
-	  case SDL_WINDOWEVENT_CLOSE:
-	    quit = true;
-	    break;
-	  default:
-	    ; /* Do nothing */
-	  }
-	case SDL_KEYDOWN:
-	  switch (event.key.keysym.sym) {
-	  case SDLK_ESCAPE:
-	    quit = true;
-	    break;
-	  case SDLK_c:
-	    cats = true;
-	    break;
-	  default:
-	    ; /* Do nothing */
-	  }
-	default:
-	  ; /* Do nothing */
-	}
-      }
-      if (quit == true) {
-	break;
-      }
-      SDL_SetRenderDrawColor(renderer,
-			     rgb_rainbow[i][0],
-			     rgb_rainbow[i][1],
-			     rgb_rainbow[i][2], 255);
-      SDL_RenderClear(renderer);
-      
-      if (cats == true) {
-	SDL_RenderCopy(renderer, texture, NULL, NULL);
-	SDL_RenderPresent(renderer);
-	SDL_Delay(1000);
-	cats = false;
-      } else {
-	SDL_RenderPresent(renderer);
-	SDL_Delay(500);
-      }
-
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+                "Couldn't initialize SDL: %s", SDL_GetError());
+        return(3);
     }
-  }
+    if (SDL_CreateWindowAndRenderer(640,640,
+                SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+                "Couldn't create window and renderer: %s", SDL_GetError());
+        return(3);
+    }
 
-  SDL_DestroyTexture(texture);
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
+    surface = SDL_LoadBMP("img/cat.bmp");
+    if (!surface) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+                "Couldn't create surface from image: %s", SDL_GetError());
+        return(3);
+    }
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
+    if (!texture) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+                "Couldn't create texture from surface: %s", SDL_GetError());
+    }
+    SDL_FreeSurface(surface);
 
-  SDL_Quit();
-      
-  return(0);
+    while (quit == false) {
+        for (i = 0; i < 6; ++i ) {
+            while (SDL_PollEvent(&event)) {
+                switch (event.type) {
+                    case SDL_QUIT:
+                        quit = true;
+                        break;
+                    case SDL_WINDOWEVENT:
+                        switch (event.window.event) {
+                            case SDL_WINDOWEVENT_CLOSE:
+                                quit = true;
+                                break;
+                            default:
+                                ; /* Do nothing */
+                        }
+                    case SDL_KEYDOWN:
+                        switch (event.key.keysym.sym) {
+                            case SDLK_ESCAPE:
+                                quit = true;
+                                break;
+                            case SDLK_c:
+                                cats = true;
+                                break;
+                            default:
+                                ; /* Do nothing */
+                        }
+                    default:
+                        ; /* Do nothing */
+                }
+            }
+            if (quit == true) {
+                break;
+            }
+            SDL_SetRenderDrawColor(renderer,
+                    rgb_rainbow[i][0],
+                    rgb_rainbow[i][1],
+                    rgb_rainbow[i][2], 255);
+            SDL_RenderClear(renderer);
+
+            if (cats == true) {
+                SDL_RenderCopy(renderer, texture, NULL, NULL);
+                SDL_RenderPresent(renderer);
+                SDL_Delay(1000);
+                cats = false;
+            } else {
+                SDL_RenderPresent(renderer);
+                SDL_Delay(500);
+            }
+
+        }
+    }
+
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+
+    return(0);
 }
--- c/sdl/image_keys.c
+++ c/sdl/image_keys.c
@@ -1,5 +1,10 @@
-/* images.c -- Andrew Cashner, 2016/12/22
- * Try basics of displaying multiple images with SDL2
+/* imagekeys.c -- Andrew Cashner, 2017/01/04
+ * Letter game for kids
+ * Flashes rainbow colors until key is pressed;
+ * then shows an image matching the key (e.g., a = aardvark)
+ * Put .bmp image files in local directory with alphabetic filenames
+ * like so: ./img/a.bmp, ./img/b.bmp.
+ * Can specify different directory with command-line option.
  */
 
 #include <SDL2/SDL.h>
@@ -7,17 +12,28 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <getopt.h>
 
 #define MAX_STRING 120
 
 /* Lookup tables */
-const char *error_msg[] = {
-    "Couldn't initialize SDL",
-    "Couldn't create window and renderer",
-    "Couldn't create surface from image",
-    "Couldn't create texture from surface",
-    "Unknown error\n"
+/*   Command-line messages */
+const enum {
+    HELP,
+    VERSION
+} cli_message_types;
+const char *cli_message[] = {
+    /* HELP */
+    "imagekeys -- Colorful letter-typing game for children\n"
+     "By Andrew cashner, 2017\n"
+     "Usage: image_keys [-d IMAGE_DIRECTORY]\n"
+     "Default image directory is ./img/\n",
+
+    /* VERSION */
+    "imagekeys v1.0\n"
 };
+
+/*   SDL error messages */
 const enum {
     INITIALIZE_ERROR,
     WINDOW_RENDERER_ERROR,
@@ -25,20 +41,29 @@ const enum {
     TEXTURE_ERROR,
     UNKNOWN_ERROR
 } error_types;
+const char *error_msg[] = {
+    "Couldn't initialize SDL",
+    "Couldn't create window and renderer",
+    "Couldn't create surface from image",
+    "Couldn't create texture from surface",
+    "Unknown error\n"
+};
+
 
 /* Function prototypes */
 void exit_SDL_error(int error_code);
 void display_picture(char *image_filename, SDL_Renderer *renderer);
 
 
-int main(void)
+int main(int argc, char *argv[])
 {
     SDL_Window *window;
     SDL_Renderer *renderer;
     SDL_Event event;
-
-    char image_filename[] = "img/a.bmp";
-    int image_filename_directory_length = 4;
+    char image_filename[MAX_STRING];
+    char image_base_filename[] = "a.bmp";
+    char image_directory[MAX_STRING] = "img/"; /* Default */
+    int  image_directory_length = 4;
 
     bool quit = false;
     bool show_picture = false;
@@ -52,6 +77,36 @@ int main(void)
         {0, 0, 255},
         {75, 0, 130} 
     };
+
+    int c;
+    while ((c = getopt(argc, argv, "hvd:")) != -1) {
+        switch (c) {
+            case 'h':
+                printf("%s", cli_message[HELP]);
+                exit(0);
+                break;
+            case 'v':
+                printf("%s", cli_message[VERSION]);
+                exit(0); 
+                break;
+            case 'd':
+                strcpy(image_directory, optarg);
+                image_directory_length = strlen(image_directory);
+                break;
+            default:
+                printf("%s", cli_message[HELP]);
+                exit(EXIT_FAILURE);
+        }
+    }
+    if (argc != optind) {
+        printf("%s", cli_message[HELP]);
+        exit(EXIT_FAILURE);
+    }
+
+    strcpy(image_filename, image_directory);
+    strcat(image_filename, image_base_filename);
+
+    /* TODO Check if directory and all bmp files a-z are present */
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         exit_SDL_error(INITIALIZE_ERROR);
@@ -84,7 +139,7 @@ int main(void)
                         } else if (event.key.keysym.sym >= SDLK_a &&
                                 event.key.keysym.sym <= SDLK_z) {
                             /* Select image file based on key pressed */
-                            image_filename[image_filename_directory_length]
+                            image_filename[image_directory_length]
                                 = (char)(event.key.keysym.sym - SDLK_a + 'a');
                             show_picture = true;
                             break;
