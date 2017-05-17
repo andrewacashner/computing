74cc6b595dae4bf75d4dc32c248ca228fc811af6
Andrew A. Cashner <andrewacashner@gmail.com>
2017-05-17 14:32:03 -0700

    sieve of eratosthenes concepts: abandon ship!

--- scheme/primes.scm
+++ scheme/primes.scm
@@ -1,19 +1,27 @@
+(use-modules (srfi srfi-1))
 ;; List primes up to given max using Sieve of Eratosthenes
-(define (unmarked minimum maximum increment ls) 
-  "Create list of all integers from minimum to maximum"
-  (if (> minimum maximum) 
+(define (unmarked minm maxm increment ls) 
+  "Create list of all integers from minm to maxm separated by increment"
+  (if (> minm maxm) 
     ls 
-    (unmarked (+ increment minimum) maximum increment (append ls (list minimum)))
-    ))
-(define (delete item ls)
-  "Remove one item from list"
-  (filter 
-    (lambda (x)
-      (not (equal? x item)))
-    ls))
+    (unmarked (+ increment minm) maxm increment (append ls (list minm)))))
+(define all (unmarked 2 100 1 '()))
+(define twos (unmarked 2 100 2 '()))
+(define next (lset-difference eq? all (cdr twos)))
+(define primes (list (car next)))
+(define next-multiples 
+  (let ([test (car (lset-difference eq? next primes))]) 
+    (unmarked test 100 test '())))
+(set! next (lset-difference eq? next (cdr next-multiples)))
+(set! primes (append primes (list (car (lset-difference eq? next primes)))))
+(define fives (unmarked 5 100 5 '()))
+(set! next (lset-difference eq? next (cdr fives)))
+(set! primes (append primes (list (car (lset-difference eq? next primes)))))
+(display primes)
+(newline)
 
-;;; start:
-;; (define ls (unmarked 2 max 1 '()))
-;;; filter for multable of each num
+;; this can't be the best way to do this!
+;; see multiple solutions on Rosetta Code
+
 
 
