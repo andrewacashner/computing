e903b1944abcbea2041384329c783c8035b94f17
Andrew A. Cashner <andrewacashner@gmail.com>
2017-06-21 21:03:03 -0500

    Improving and adding: extracting XML values from LY

--- scheme/notes.scm
+++ scheme/notes.scm
@@ -12,63 +12,68 @@
 ;;           <type>quarter</type>
 ;;         </note>
 
-; (define parse-ly-note
-;   (lambda (note) 
-;     (let* ([chars (string->list note)]
-;            [len (- 1 (length chars))]
-;            [step (char-upcase (car chars))]
-;            [duration (list-ref chars len)]
-;            [octave (list-ref chars (- 1 len))])
-;   ; ... )))
-;   ;; make these separate functions
- 
 (define step
   (lambda (note)
+    "Given a string with a single Lilypond note command,
+    return an uppercase char for the pitch name"
     (char-upcase (car (string->list note)))))
 
-(define duration
+(define ly-duration
   (lambda (note)
-    (cadr (string->list note)))) ; NO
+    "Given Lilypond note command, extract the concluding numeric portion of the
+    string and convert to integer"
+    (string->number
+              (substring note (string-index note char-set:digit)))))
 
-;; TODO deal with double-accidentals
+(define duration
+  (lambda (note beats)
+    "Given Lilypond note command and number of beats per measure, 
+    return integer for XML duration value" 
+    (/ beats (ly-duration note))))
+
+(define type 
+  (lambda (note)
+    (let ([dur (ly-duration note)])
+      (cond [(= dur 1) "whole"]
+            [(= dur 2) "half"]
+            [(= dur 4) "quarter"]
+            [(= dur 8) "eighth"]
+            [(= dur 16) "sixteenth"]
+            [else #f]))))
+; TODO surely there's a better way, e.g. table lookup
+
 (define alter
   (lambda (note)
+    "Given Lilypond note command string, return integer with number of half
+    steps to adjust pitch"
     (cond
-      [(string-contains note "es") -1] 
-      [(string-contains note "is") 1]
-      [else #f])))
+      [(string-contains note "eses") -2]
+      [(string-contains note "es")   -1] 
+      [(string-contains note "isis")  2]
+      [(string-contains note "is")    1]
+      [else 0])))
 
 (define octave
   (lambda (note)
-    (let ([ls (string->list note)])
-      (letrec 
-        ([oct-calc 
-           (lambda (ls oct) 
-             (if (null? ls) 
-               oct
-               (cond [(eqv? #\' (car ls))
-                      (oct-calc (cdr ls) (+ oct 1))]
-                     [(eqv? #\, (car ls))
-                      (oct-calc (cdr ls) (- oct 1))]
-                     [else (oct-calc (cdr ls) oct)])))])
-        (oct-calc ls 4)))))
+    (let ([octave-increment
+            (cond [(string-contains note "'") 
+                   (string-count note #\')]
+                  [(string-contains note ",")
+                   (- 0 (string-count note #\,))]
+                  [else 0])])
+      (+ 4 octave-increment))))
 
-;(define note:ly->xml
-;  (lambda (lynote)
-;    (let*
-;      ([lychars     (string->list lynote)]
-;       [lystep      (char-upcase (car lychars))] 
-;       [lyoctave    (octave->duration (list-ref lychars 1))] 
-;       ; ignoring accidentals for now, assuming there is an octave sign
-;       [lyduration  (cadr lychars)]
-;       [lytype      (duration->type lyduration)])
-;      (xml note
-;        (xml pitch
-;          (xml step lystep)
-;          (xml octave lyoctave))
-;          (xml duration lyduration)
-;          (xml type lytype)))))
-;; better to make a list/data structure for xml note?
+(define note:ly->xml
+  (lambda (lynote)
+    (xml note 
+         (((xml pitch
+              (xml step (step lynote))
+              (xml octave (octave lynote))
+              (xml alter (alter lynote))) 
+         (xml duration (duration lynote))
+         (xml type (type (duration lynote))))))))
+; TODO abbreviate with lets
+; TODO add type
 
 (define xml
   (lambda (element contents)
