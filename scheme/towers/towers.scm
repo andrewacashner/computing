2d8b569d302c5a279077370395d36e48f8fc6ece
Andrew A. Cashner <andrewacashner@gmail.com>
2017-09-18 11:02:22 -0400

    conditionals for testing tower moves

--- scheme/towers/towers.scm
+++ scheme/towers/towers.scm
@@ -9,18 +9,61 @@
            (null? ls)]
           [(eqv? msg 'top)
            (if (null? ls)
-             "empty list" 
+             'empty
              (car ls))]
            [(eqv? msg 'push!)
             (set! ls (cons (car args) ls))]
           [(eqv? msg 'pop!)
            (if (null? ls)
-             "empty list"
+             'empty
              (set! ls (cdr ls)))]
           [else "unknown command"])))))
+
+(define stack-init
+  (lambda (stack n)
+    (if (> 1 n)
+      0 
+      (begin
+        (stack 'push! n) 
+        (stack-init stack (- n 1))))))
+
+(define check-position
+  (lambda (a b)
+    (or (eqv? b 'empty)
+        [and (not (eqv? a 'empty))
+             b 
+             (or 
+               (and (even? a) (odd? b)) 
+               (and (odd? a) (even? b))) 
+             (< a b)])))
+
+(define pop-swap
+  (lambda (stack-a stack-b)
+    (let ([a (stack-a 'top)]
+          [b (stack-b 'top)])
+      (if (check-position a b) 
+        (begin 
+          (stack-b 'push! a) 
+          (stack-a 'pop!))
+        #f))))
 
 (define stack0 (make-stack))
 (define stack1 (make-stack))
 (define stack2 (make-stack))
 
+(stack-init stack0 5)
 
+(define tower-status
+  (lambda (stack0 stack1 stack2) 
+    (display
+      (list
+      (stack0 'top)
+      (stack1 'top)
+      (stack2 'top)))
+    (newline)))
+
+(pop-swap stack0 stack1)
+(pop-swap stack0 stack2)
+(pop-swap stack1 stack2)
+(pop-swap stack0 stack1)
+(tower-status stack0 stack1 stack2)
