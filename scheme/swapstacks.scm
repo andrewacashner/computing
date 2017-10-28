1caa7f5e31638c92d66b65c1aba37b196b15647c
Andrew A. Cashner <andrewacashner@gmail.com>
2017-10-28 11:04:33 -0400

    fixes not all way

--- scheme/swapstacks.scm
+++ scheme/swapstacks.scm
@@ -21,23 +21,19 @@
            (list-ref ls (car args))]
           [(eq? msg 'length)
            (length ls)]
+          [(eq? msg 'print)
+           (begin 
+             (display ls)
+             (newline))]
           [else "Unknown message"])))))
 
 
-(define stackA (make-stack))
-(define stackB (make-stack))
-
-(stackA 'push! 0)
-(stackA 'push! 1)
-(stackA 'push! 2)
-(stackA 'push! 3)
-(stackA 'push! 4)
-
-(stackB 'push! 'a)
-(stackB 'push! 'b)
-(stackB 'push! 'c)
-(stackB 'push! 'd)
-(stackB 'push! 'e)
+(define push-list!
+  (lambda (stack ls)
+    (if (not (null? ls))
+      (begin
+        (stack 'push! (car ls))
+        (push-list! stack (cdr ls))))))
 
 (define dump-stack! 
   (lambda (s1 s2)
@@ -51,26 +47,41 @@
         (dump (s1 'top) s2))))))
 
 (define dump-stack-ref! 
-  (lambda (s1 n s2)
+  (lambda (s1 i s2)
     "Pop given number of items from stack1 and push onto stack2"
-    (let dump ([n n]
+    (let dump ([n i]
                [s1-top (s1 'top)] 
                [s2 s2])
       (if (and
-            (> 0 n) 
+            (> n 0) 
             (not (null? s1-top)))
         (begin
           (s2 'push! s1-top) 
           (s1 'pop!)
-          (dump (- 1 n) (s1 'top) s2))))))
+          (dump (- n 1) (s1 'top) s2))))))
 
+(define s3 (make-stack))
 (define swap-stacks!
   (lambda (s1 s2)
-    (let ([s3 (make-stack)]
-          [s2-len (s2 'length)])
+    (let ([s2-len (s2 'length)])
       (begin
         (dump-stack! s1 s3)
         (dump-stack! s2 s3)
-        (dump-stack-ref! s3 s2-len s2)
+        (dump-stack-ref! s3 s2-len s1)
         (dump-stack! s3 s2)))))
 
+(define stackA (make-stack))
+(define stackB (make-stack))
+(define numbers '(0 1 2 3 4))
+(define letters '(a b c d e))
+(push-list! stackA numbers)
+(push-list! stackB letters)
+
+(display "Initial state:") (newline)
+(display "A:") (stackA 'print) (newline)
+(display "B:") (stackB 'print) (newline)
+;
+; (swap-stacks! stackA stackB)
+; (display "Final state:") (newline)
+; (display "A:") (stackA 'print) (newline)
+; (display "B:") (stackB 'print) (newline)
