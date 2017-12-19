18c5f8b2e69ee8f0c85135b6b961d6e0475273c2
Andrew A. Cashner <andrewacashner@gmail.com>
2017-12-19 10:00:49 -0500

    refactor

--- scheme/solfa.scm
+++ scheme/solfa.scm
@@ -1,46 +1,36 @@
 (use-modules
   (ice-9 format))
 
-(define scale '(ut re mi fa sol la))
+(define scale '(ut re mi fa sol la)
 
-(define gamut
-  '((C sol fa ut)
-    (D la sol re)
-    (E #f la mi)
-    (F ut #f fa)
-    (G re ut sol)
-    (A mi re la)
-    (B-mol fa #f #f)
-    (B-dur #f mi #f)))
+(define gamut '((C sol fa ut) 
+                (D la sol re) 
+                (E #f la mi) 
+                (F ut #f fa) 
+                (G re ut sol) 
+                (A mi re la) 
+                (B-mol fa #f #f) 
+                (B-dur #f mi #f)))
 
 (define member-index
   (lambda (key ls)
     (let ([tail (member key (reverse ls))])
       (and tail (length (cdr tail))))))
 
-(define solfa->degree
-  (lambda (solfa)
-    (member-index solfa scale)))
-
-(define degree->solfa
-  (lambda (degree)
-    (list-ref scale degree)))
-
 (define pitch
   (lambda (letter)
-    (let* ([hexachords   (assoc-ref gamut letter)]
-           [hex-true     (filter identity hexachords)]
-           [name-degrees (map solfa->degree hex-true)]
-           [sort-degrees (sort name-degrees >)]
-           [solfa-ls     (map degree->solfa sort-degrees)]
-           [solfa-ls-str (map symbol->string solfa-ls)]
-           [solfa-output (string-join solfa-ls-str ", ")]
+    (let* ([hexachord   (filter identity (assoc-ref gamut letter))]
+           [name-degree (map (lambda (x) member-index x scale) hexachord)]
+           [sort-degree (sort name-degree >)]
+           [solfa       (map (lambda (x) (list-ref scale x)) sort-degree)]
+           [solfa-str   (map symbol->string solfa)]
+           [solfa-fmt   (string-join solfa-str ", ")]
            [letter-output 
              (if (or (eq? letter 'B-dur) 
                      (eq? letter 'B-mol))
                "B"
                (symbol->string letter))])
-      (format #f "~a (~a)" letter-output solfa-output))))
+      (format #f "~a (~a)" letter-output solfa-fmt))))
 
 (define solfa-hex
   (lambda (letter hex)
