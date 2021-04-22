bf6ea73f3e98aec72140bc3d9c6f9861bb8b29bf
Andrew A. Cashner <andrewacashner@gmail.com>
2021-04-22 13:59:05 -0400

    back to mixal

---
+++ c/daily/max.c
@@ -0,0 +1,15 @@
+/* max.c
+ *
+ * Given a series of any length of numerals, separated by spaces, find the
+ * maximum
+ */
+
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+
+
+
+    return(0);
+}
