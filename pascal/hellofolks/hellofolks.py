fc31cb8c289ab7e8c321b893babf495938556b39
Andrew A. Cashner <andrewacashner@gmail.com>
2021-10-01 14:53:26 -0400

    language comparison

---
+++ pascal/hellofolks/hellofolks.py
@@ -0,0 +1,11 @@
+names = ['Joseph'
+        , 'José'
+        , 'Josephine'
+        , 'Giuseppe'
+        , 'Iosephus'
+        , 'Josefina'
+        , 'Josephine']
+
+for name in names:
+    print("Hello, " + name + "!")
+
---
+++ pascal/hellofolks/hellofolks.sh
@@ -0,0 +1,7 @@
+names=('Joseph' 'José' 'Josephine' 'Giuseppe' 'Iosephus' 'Josefina'
+    'Josephine')
+
+for i in ${names[@]}; do 
+    echo "Hello, $i!";
+done
+
