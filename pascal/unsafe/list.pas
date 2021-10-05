a647332df32f27b175c2940de2dbe12b040152b1
Andrew A. Cashner <andrewacashner@gmail.com>
2021-10-05 12:10:02 -0400

    working free-list function, not print-list function

--- pascal/unsafe/list.pas
+++ pascal/unsafe/list.pas
@@ -1,4 +1,6 @@
 {$mode objfpc}{$H+}{$J-}
+{$optimization tailrec}
+
 {******************************** 
   Linked list with Pitch class
   AAC, 2021/10/02
@@ -88,7 +90,6 @@ constructor TNode.Create(Pitch: TPitch; PNode: TPNode);
 begin
   FPitch := Pitch;
   FPNodeNext := PNode;
-  WriteLn('Creating new node with pitch ' + FPitch.ToString);
 end;
 
 function Last(List: TPNode): TPNode;
@@ -99,7 +100,6 @@ begin
   end
   else
   begin
-    WriteLn('Last: going to next node');
     result := Last(List^.FPNodeNext);
   end;
 end;
@@ -108,31 +108,25 @@ function AddNode(Head: TPNode; Pitch: TPitch): TPNode;
 { create a new node and add it to the beginning of a given list }
 var
   NewNode: TNode;
-  PNewNode: TPNode;
 begin
-  NewNode := nil;
-  PNewNode := nil;
-
-  try
-    NewNode := TNode.Create(Pitch, Head);
-    WriteLn('Added new node with pitch ' + NewNode.FPitch.ToString);
-    PNewNode := @NewNode;
-  finally
-    result := PNewNode;
-  end;
+  NewNode := TNode.Create(Pitch, Head);
+  result := @NewNode;
 end;
 
 procedure PrintList(Head: TPNode);
+var
+  PNextNode: TPNode;
 begin
   if Head = nil then
   begin 
-    writeln();
+    writeln(); 
+    exit;
   end
   else
   begin
-    WriteLn('Trying to print list...');
     Write(Head^.FPitch.ToString + ' ');
-    PrintList(Head^.FPNodeNext);
+    PNextNode := Head^.FPNodeNext;
+    PrintList(PNextNode);
   end;
 end;
 
@@ -142,17 +136,13 @@ var
 begin
   if Head = nil then
   begin
-    WriteLn('FreeList: reached end of list');
-    WriteLn('FreeList: Freeing node...');
-    FreeAndNil(Head);
     exit;
   end
   else
   begin
-    WriteLn('FreeList: Freeing head.FPitch ...');
-    FreeAndNil(Head^.FPitch);
-    WriteLn('FreeList: Freeing next node...');
     PNextNode := Head^.FPNodeNext;
+    Head^.FPitch.Destroy;
+    FreeAndNil(Head);
     FreeList(PNextNode);
   end;
 end;
@@ -165,14 +155,12 @@ begin
 
   try
     PList := AddNode(PList, TPitch.Create(pkC, 4, akSh));
-    {
     PList := AddNode(PList, TPitch.Create(pkG, 3, akFl));
     PList := AddNode(PList, TPitch.Create(pkC, 4, akSh));
     PList := AddNode(PList, TPitch.Create(pkC, 4, akSh));
     PList := AddNode(PList, TPitch.Create(pkG, 3, akFl));
     PList := AddNode(PList, TPitch.Create(pkC, 4, akSh));
-    PrintList(PList);
-    }
+    {    PrintList(PList); }
 
   finally
     FreeList(PList);
