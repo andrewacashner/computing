16f5dd56e7bd06a368182e229e59b6c6f5931ed7
Andrew A. Cashner <andrewacashner@gmail.com>
2021-10-07 20:11:52 -0400

    try parsing a string for a macro definition

--- pascal/ly/lymacro.pas
+++ pascal/ly/lymacro.pas
@@ -1,26 +1,68 @@
-{ lymacro
+{ 
+  `lymacro`
+
   Andrew Cashner, 2021/10/07
+ 
   A basic macro-substitution program targeting Lilypond code.  
+  Lilypond allows users to define macros like this:
+  
+  ~~~~
+  MusicSoprano = { c''4 d''4 ees''4 }
+  LyricsSoprano = \lyricmode { ly -- ric text }
+  ~~~~
+  
+  Then they can call them like `\MusicSoprano` or `\LyricsSoprano`.
+  Instead of actually processing the arguments like Lilypond does, we just
+  want to do simple textual substitution of the argument for the macro label.
 }
 
 {$mode objfpc}{$H+}{$J-}
 program lymacro(input, output);
 
-uses SysUtils, Classes, Generics.Collections;
+uses SysUtils, Classes, Generics.Collections, Strings;
+
+function ExtractMacro(S: String): TMacroKeyValue;
+const
+  Delim: String := '='
+var
+  KeyLabel, Value: String;
+  EndKeyLabel, StartValueText: Integer;
+  Macro: TMacroKeyValue;
+begin
+  Value := StrRScan(S, Delim));
+  KeyLabel := LeftStr(S,LastDelimiter(Delim, S) - 1);
+  if Value <> nil then
+    Value := Trim(Value);
+  if KeyLabel <> nil then
+    KeyLabel := Trim(KeyLabel);
+
+  try
+    Macro := TMacroKeyValue.Create(KeyLabel, Value);
+  finally 
+    result := Macro;
+  end;
+end;
 
 type
-  TMacroDict = specialize TDictionary<String, String>;
+  TMacroKey      = specialize TKey<String>;
+  TMacroValue    = specialize TValue<String>;
+  TMacroKeyValue = specialize TPair<TMacroKey, TMacroValue>;
+  TMacroDict     = specialize TDictionary<TMacroKeyValue>;
 
-  { MAIN }
+{ MAIN }
 var
   Macros: TMacroDict;
+  InputText: String = 'MusicSoprano = { c''4 d''4 es''4 }';
   V: String;
 begin
   Macros := TMacroDict.Create();
-  Macros.Add('MusicSoprano', '{ c''4 d''4 es''4 }');
-  Macros.Add('LyricsSoprano', '\lyricmode { ly -- ric text }');
-  Macros.TryGetValue('MusicSoprano', V);
-  WriteLn(V);
-  FreeAndNil(macros);
+  try
+    Macros.Add('MusicSoprano', '{ c''4 d''4 es''4 }');
+    Macros.Add('LyricsSoprano', '\lyricmode { ly -- ric text }');
+    Macros.TryGetValue('MusicSoprano', V);
+  finally
+    WriteLn(V);
+    FreeAndNil(macros);
+  end;
 end.
 
