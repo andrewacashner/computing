382e9f6814738152b2f58e2e7894523ed481ce30
Andrew A. Cashner <andrewacashner@gmail.com>
2023-05-12 14:21:31 -0400

    try more advanced args, conditionals, partial

--- clojure/first.clj
+++ clojure/first.clj
@@ -1,23 +1,6 @@
 ; First Clojure try, 2023/05/12
 (require '[clojure.string :as str])
 
-(def hello "Hello")
-
-(defn greeting
-  "Return string with hello message to `name`"
-  [name]
-  (str hello ", " name "!"))
-
-(defn name-list
-  "Insert commas between list of names"
-  [ls]
-  (str/join ", " ls))
-
-(defn group-greeting
-  "Return a string with a greeting message to a list of names without Oxford comma"
-  [names]
-  (greeting (name-list names)))
-
 (defn join-with-ending
   "Insert `delim` between each item, and `ending` before last"
   [delim ending items]
@@ -35,5 +18,24 @@
   "Return a string with a greeting message to a list of names with Oxford comma"
   [salutation names]
   (str salutation ", " (oxford-name-list names) "!"))
+
+; Use `& arg` to package remaining args as list
+(defn greeting
+  "Greet one or more people by name
+    Input:  (1) String message, (2) List of string names
+    Returns: String"
+  [msg & people] 
+  (let [people-count (count people)]
+  (cond
+    (= people-count 0) (str msg "!")
+    (= people-count 1) (str msg ", " (first people))
+    (< people-count 3) (str msg ", " (str/join " and " people) "!")
+    (>= people-count 3) (oxford-greeting msg people)))) 
+
+(def hello-str "Hello")
+
+; Use def and partial to create partial function; avoid extra processing of
+; args
+(def hello (partial greeting hello-str))
 
 
