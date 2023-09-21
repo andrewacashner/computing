db29e615fa080be29a6a700ced1663fc9ce967d5
Andrew A. Cashner <andrewacashner@gmail.com>
2023-09-21 13:01:00 -0400

    working basic timeline game with yaml input; using leiningen

---
+++ clojure/timeline/project.clj
@@ -0,0 +1,14 @@
+(defproject timeline "0.1.0-SNAPSHOT"
+  :description "Timeline game"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [
+                 [org.clojure/clojure "1.11.1"]
+                 [clj-commons/clj-yaml "1.0.27"]
+                 [clj-time "0.15.2"]
+                 ]
+  :main ^:skip-aot timeline.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
