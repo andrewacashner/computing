ddcc9eb2f0b485d6079718c8b7e9a36df0eedc84
Andrew A. Cashner <andrewacashner@gmail.com>
2023-09-28 08:23:56 -0400

    guess a randomly chosen answer from list; shuffle list

--- clojure/seesawFirst/src/seesawFirst/core.clj
+++ clojure/seesawFirst/src/seesawFirst/core.clj
@@ -3,78 +3,79 @@
 
 (ss/native!)
 
-(def nameList (ss/listbox :model ["Andrew" "Ann" "Ben" "Joy"]))
+(def Cashners ["Andrew" "Ann" "Ben" "Joy"])
 
-(def Names ["Matthew" "Mark" "Luke" "John"])
-
-(defn nameCard
-  [name-str]
-  (ss/text name-str))
-
-(def Answer (atom ()))
-
-(defn set-click-alert
-  [object]
-  (ss/listen object :mouse-clicked
-             (fn [e] (if (= (:text e) @Answer)
-                      (ss/alert e "You got it!")
-                      (ss/alert e "Try again!")))))
-
-(def NameCards (map nameCard Names))
-
-(doseq [x NameCards] (set-click-alert x))
-
-(defn nameDeck
-  [names]
-  (ss/flow-panel :items names))
-
-(def NameDeck (nameDeck NameCards))
+(def nameList (ss/listbox :model Cashners))
 
 (defn choose-answer
   [items]
   (rand-nth items))
 
-(def addOne   (ss/button :text "+1"))
-(def minusOne (ss/button :text "-1"))
+
+(def button-plus-one   (ss/button :text "+1"))
+(def button-minus-one  (ss/button :text "-1"))
 
 (defn score-panel
   [n]
-  (ss/vertical-panel :items [(format "Score: %d" n) addOne minusOne]))
+  (ss/vertical-panel 
+    :items [(format "Score: %d" n) 
+            button-plus-one 
+            button-minus-one]))
+
+(def button-shuffle (ss/button :text "Shuffle"))
 
 (defn frame-contents 
   [n]
   (ss/border-panel 
-    :west (score-panel n) 
-    :center NameDeck
-    :east nameList))
+    :west (ss/vertical-panel :items [nameList button-shuffle])
+    :east (score-panel n)))
 
 (def f (ss/frame :title "Game"
                  :content (frame-contents 0)
                  :on-close :exit))
 
 (defn adjust-score 
-  [this_atom this_frame func] 
-  (ss/config! this_frame 
-              :content (frame-contents (swap! this_atom func))))
+  [this-atom this-frame func] 
+  (ss/config! this-frame 
+              :content (frame-contents (swap! this-atom func))))
 
 (def Score (atom 0))
 
 (defn inc-Score! [] (adjust-score Score f inc))
 (defn dec-Score! [] (adjust-score Score f dec))
 
-(ss/listen addOne   :action (fn [e] (inc-Score!)))
-(ss/listen minusOne :action (fn [e] (dec-Score!)))
+(ss/listen button-plus-one :action (fn [e] (inc-Score!)))
 
-(ss/listen nameList :mouse-clicked
-           (fn [e] (if-let [choice (ss/selection e)]
-                     (when (= choice "Andrew") (inc-Score!)))))
+(ss/listen button-minus-one :action (fn [e] (dec-Score!)))
 
+(defn set-name-listener
+  [this-name]
+  (ss/listen nameList :mouse-clicked
+             (fn [e] (if-let [choice (ss/selection e)]
+                     (if (= choice this-name) 
+                       (inc-Score!)
+                       (dec-Score!))))))
 
+(defn reset-list-model
+  [object ls]
+  (ss/config! object :model ls))
+
+(defn reset-namelist
+  [names]
+  (reset-list-model nameList names))
+
+(defn set-shuffle-listener
+  [names answer]
+  (ss/listen button-shuffle :action 
+             (fn [e] 
+               (reset-namelist (shuffle names)))))
 
 (defn -main
   [& args]
-  (do
-    (reset! Answer (choose-answer Names))
+  (let [Names Cashners
+        Answer (choose-answer Names)]
+    (set-name-listener Answer)
+    (set-shuffle-listener Names Answer)
     (ss/invoke-later
       (-> f
           ss/pack!
