d269e9897bbf81bd0d53afdf00c1c7ce2da91e44
Andrew A. Cashner <andrewacashner@gmail.com>
2023-09-29 11:10:23 -0400

    reimplement timeline with model-view-controller model

--- clojure/seesawFirst/src/seesawFirst/core.clj
+++ clojure/seesawFirst/src/seesawFirst/core.clj
@@ -1,7 +1,167 @@
 (ns seesawFirst.core
-  (:require [seesaw.core :as ss]))
+  (:require [seesaw.core :as ss])
+  (:gen-class))
 
 (ss/native!)
+
+(defrecord Event [date description])
+
+(def Default-timeline (map map->Event 
+                        [{:date "1981-04-11"
+                          :description "Andrew born"}
+                         {:date "1983-03-24"
+                          :description "Ann born"}
+                         {:date "2011-01-16"
+                          :description "Ben born"}
+                         {:date "2014-03-10"
+                          :description "Joy born"}]))
+
+(defn show-event
+  [event]
+  (:description event))
+
+(defn sort-timeline
+  [Timeline]
+  (swap! Timeline update :timeline #(sort-by :date %)))
+
+(defn create-view-children
+  [Timeline]
+  (let [instructions (ss/label :text "Click on the earliest event")
+
+        timeline-box 
+        (ss/listbox :model (map show-event (:timeline @Timeline))
+                    :listen [:mouse-clicked
+                             (fn [e] 
+                               (if-let [choice (ss/selection e)]
+                                (if (= choice 
+                                       (show-event (:earliest @Timeline)))
+                                 (ss/alert e "You got it!")
+                                 (ss/alert e "Try again!"))))])
+    
+        sort-button 
+        (ss/button :text "Sort"
+                   :listen [:action (fn [e] (sort-timeline Timeline))])]
+
+    (ss/vertical-panel :items [instructions timeline-box sort-button])))
+
+(defn create-view
+  [children]
+  (ss/frame :title "Timeline"
+            :visible? true
+            :content children
+            :on-close :exit
+            :width 400
+            :height 800))
+
+(defn update-view
+  [view Timeline]
+  (fn [_ _ _ New-timeline]
+      (ss/config! view :content (create-view-children Timeline))))
+
+(defn state-logger
+  [_ _ _ new-state]
+  (println "State changed to: " new-state))
+
+(defn -main
+  [& args]
+  (let [master-timeline (sort-by :date Default-timeline)
+        Timeline (atom {:timeline (shuffle master-timeline)
+                        :earliest (first master-timeline)})
+        view (create-view (create-view-children Timeline))]
+   
+    (add-watch Timeline :update-view (update-view view Timeline))
+    (add-watch Timeline :logger state-logger)
+    
+    (ss/invoke-now
+      (-> view ss/pack! ss/show!))))
+
+(comment
+  ; re-implementation of increment adder (below) using master-view-controller
+  ; model (after online tutorial)
+(defn change-score
+  [model change-fn]
+  (swap! model update :score #(change-fn %)))
+
+(defn reset-family 
+  [model]
+  (swap! model update :family #(shuffle %)))
+
+(defn create-view-children
+  [score family state score-change-fn family-change-fn]
+  (let [score-label (ss/label :text "SCORE")
+        score-value (ss/label :text score)
+
+        score-box (ss/vertical-panel :items [score-label score-value])
+
+        plus-button (ss/button :text "+1"
+                               :listen [:action 
+                                        (fn [e] (score-change-fn state inc))])
+        minus-button (ss/button :text "-1"
+                               :listen [:action 
+                                        (fn [e] (score-change-fn state dec))])
+        button-panel (ss/grid-panel :rows 1 :columns 2
+                                    :items [plus-button minus-button])
+
+        score-panel (ss/border-panel
+                      :items [[score-box :north] [button-panel :south]])
+
+        family-list (ss/listbox :model family)
+        shuffle-button (ss/button :text "Shuffle"
+                                  :listen [:action
+                                           (fn [e] (family-change-fn state))])
+        family-panel (ss/border-panel
+                      :items [[family-list :north] [shuffle-button :south]])]
+    
+    (ss/border-panel
+      :items [[family-panel :west] [score-panel :east]])))
+
+(defn create-view
+  [children]
+  (ss/frame :title "Increment Calculator"
+            :visible? true
+            :content children
+            :on-close :exit
+            :width 400
+            :height 300))
+
+(defn update-view
+  [view state]
+  (fn [_ _ _ new-state]
+    (let [score (:score new-state)
+          family (:family new-state)]
+      (ss/config! view :content (create-view-children score 
+                                                      family
+                                                      state 
+                                                      change-score
+                                                      reset-family)))))
+
+(defn state-logger
+  [_ _ _ new-state]
+  (println "State changed to: " new-state))
+
+(def Cashners ["Andrew" "Ann" "Ben" "Joy"])
+
+(defn -main
+  [& args]
+  (let [init-score 0
+        init-family Cashners
+        init-winner (rand-nth init-family)
+        state (atom {:score init-score
+                     :family init-family
+                     :winner init-winner})
+        view (create-view (create-view-children init-score 
+                                                init-family 
+                                                state 
+                                                change-score
+                                                reset-family))]
+    (add-watch state :update-view (update-view view state))
+    (add-watch state :logger state-logger)
+    (ss/invoke-now (fn [] (ss/show! (ss/pack! view))))))
+)
+(comment
+  ; original mishmash timeline+increment adder mockup
+
+
 
 (def Cashners ["Andrew" "Ann" "Ben" "Joy"])
 
@@ -72,6 +232,7 @@
 
 (defn -main
   [& args]
+  (ss/native!)
   (let [Names Cashners
         Answer (choose-answer Names)]
     (set-name-listener Answer)
@@ -80,3 +241,4 @@
       (-> f
           ss/pack!
           ss/show!))))
+)
