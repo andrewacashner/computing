21c7d317554e316e6bf10de7d1f5c1973b9da43f
Andrew A. Cashner <andrewacashner@gmail.com>
2024-01-09 08:20:07 -0500

    remove non-working drag-and-drop interface

--- clojure/timelinegui/src/timelinegui/core.clj
+++ clojure/timelinegui/src/timelinegui/core.clj
@@ -100,31 +100,6 @@
       (reset! state new-data))))
 
 
-;from seesaw github seesaw/test/examples/xyz_panel.clj
-(defn moveable
-  [widget]
-  (let [start-point (java.awt.Point.)]
-    (sb/when-mouse-dragged 
-      widget
-      :start (fn [e] (ss/move! e :to-front)
-               (.setLocation start-point (.getPoint e)))
-      :drag (fn [e _]
-              (let [p (.getPoint e)]
-                (ss/move! e :by [(- (.x p) (.x start-point))
-                                 (- (.y p) (.y start-point))])))
-      :finish (fn [e] 
-                (let [p (.getPoint e)]
-                  (ss/alert e (format "Dragged to (%d, %d)" (.x p) (.y p)))
-                  ; check if mouse release spot is inside correct box
-                  ; or send click or other message to box at this spot
-                  ; TODO use sd/default-transfer-handler from drag-and-drop
-                  ; interface to transfer info from one object to another
-                  )
-                )))
-      ; TODO move timeline cards to allow space for new card
-      ; check if correct place
-  widget)
-
 (def center-align {:halign :center})
 
 ; TODO center card text or just center the year?
@@ -138,17 +113,14 @@
 
 (defn clue-card
   [fact]
-  (moveable
   (ss/text :text (:description fact)
-           card-options)))
+           card-options))
 
 (defn fact-card
   [state fact]
   (ss/text :text (show-fact fact)
            :listen [:mouse-clicked 
-                    (partial check-and-advance state fact)
-                    :mouse-moved
-                    (fn [e] (ss/alert e "Dragged to here!"))]
+                    (partial check-and-advance state fact)]
            card-options))
 
 
