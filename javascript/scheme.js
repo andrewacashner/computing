0bd0719f6b80b771356621efbccd8e9e828ef472
Andrew A. Cashner <andrewacashner@gmail.com>
2024-02-06 14:20:13 -0500

    nearly working parser and eval function, testing functions

--- javascript/scheme.js
+++ javascript/scheme.js
@@ -22,188 +22,184 @@ class ListItem {
   }
 }
 
-class Outline {
+class Scheme {
 
-  constructor(level = 0, start = this.#unset, end = this.#unset) {
-    this.level = level;
-    this.start = start;
-    this.end = end;
-  }
+  static cons = (a, b) => new ListItem(a, b);
 
-  #unset = -1;
+  static car = item => item.data;
 
-  isStartSet() { return this.start !== this.#unset; }
-  isEndSet() { return this.end !== this.#unset; }
+  static cdr = item => item.next;
 
-  isComplete() { return this.isStartSet() && this.isEndSet(); }
-
-  within(str) {
-    return str.slice(this.start, this.end + 1);
-  }
-}
-
-const Scheme = {
-  cons: (a, b) => new ListItem(a, b),
-
-  head: item => item.data,
-
-  tail: item => item.next,
-
-  list: function (...items) {
+  static list = function (...items) {
     let head = null;
     if (items.length > 0) {
       head = Scheme.cons(items[0], Scheme.list(...items.slice(1)));
     }
     return head;
-  },
+  }
 
-  reverse: function (ls) {
+  static reverse = function (ls) {
     function reverseDo(oldLs, newLs) {
       if (!oldLs) {
         return newLs;
       } else {
-        return reverseDo(Scheme.tail(oldLs), Scheme.cons(Scheme.head(oldLs), newLs));
+        return reverseDo(Scheme.cdr(oldLs), Scheme.cons(Scheme.car(oldLs), newLs));
       }
     }
     return reverseDo(ls, null);
-  },
+  }
 
-  map: function (ls, fn) {
+  static map = function (ls, fn) {
     function mapDo(oldLs, newLs) {
       if (!oldLs) {
         return Scheme.reverse(newLs);
       } else {
-        return mapDo(Scheme.tail(oldLs), Scheme.cons(fn(Scheme.head(oldLs)), newLs));
+        return mapDo(Scheme.cdr(oldLs), Scheme.cons(fn(Scheme.car(oldLs)), newLs));
       }
     }
     return mapDo(ls, null);
-  },
-
-  eval: function (str) {
-//      if (!(sexpr.startsWith("(") && sexpr.endsWith(")"))) {
-//        throw "Syntax error";
-//      }
-    
-    let outlines = analyzeExpr(str);
-    console.log(outlines);
-
-    let values = "";
-    for (let outline of outlines) {
-      let sexpr = outline.within(str);
-     
-      let args = sexpr.slice(1, sexpr.length - 1).split(" ");
-      let fn = args.shift();
-      
-      let value = Scheme[fn](...args);
-      console.log(value);
-      values += value;
-    }
-    return values;
   }
-}
 
-// TODO you need to account for normal arguments, not just sexpressions, and
-// how to plug value of sexpr back into stream of arguments?
+  static #parse = function(str) {
 
-function analyzeExpr(str) {
-  let args = [];
-  let outlines = [];
-  let [open, close] = "()";
+    // Split a string into arguments;
+    // Arguments can be space-delimited words OR s-expressions delimited with
+    // parentheses;
+    // Return an array of strings containing either the words or the
+    // parenthetical expressions; do not parse within the parentheses.
+    function args(str) {
+      let args = [];
+      let thisArg = "";
+      let inWord = false;
+      let inExpr = false;
+      let exprLevel = 0;
 
-  for (let level = 0, i = 0; i < str.length; ++i) {
-    if (str[i] === open) {
-      let outline = new Outline(level, i);
-      outlines.push(outline);
-      ++level;
-    } else if (str[i] === close) {
-      --level;
-      let outline = outlines.pop();
-      if (!(outline.isEndSet() && outline.level === level)) {
-        outline.end = i;
-        args.push(outline);
+      for (let c of str) {
+        if (/\s/.test(c)) {
+          if (inExpr) {
+            thisArg += c;
+          } else if (inWord) {
+            args.push(thisArg);
+            inWord = false;
+            thisArg = "";
+          } else {
+            continue;
+          }
+        } else if (c === "(" && !inWord) {
+          inExpr = true;
+          inWord = false;
+          thisArg += c;
+          ++exprLevel;
+        } else if (c === ")" && inExpr) {
+          --exprLevel;
+          inWord = false;
+          thisArg += c;
+          if (exprLevel === 0) {
+            args.push(thisArg);
+            inExpr = false;
+            thisArg = "";
+          }
+        } else {
+          if (!inWord && !inExpr) {
+            inWord = true;
+          }
+          thisArg += c;
+        }
       }
+      if (thisArg) {
+        args.push(thisArg);
+      }
+      return args;
+    }
+
+    try {
+      if (str.startsWith("(") && str.endsWith(")")) {
+        let inner = str.slice(1, str.length - 1);
+        let argStrings = args(inner);
+
+        let parsed = argStrings.map((arg) => {
+          if (arg.includes("(") && arg.includes(")")) {
+            return Scheme.#parse(arg);
+          } else {
+            return arg; 
+          }
+        });
+        return parsed;
+      } else {
+        throw "Syntax error";
+      }
+    } catch(e) {
+      console.error(e);
+      return undefined;
     }
   }
 
-  return args;
+  static eval = function (str) {
+    
+    function applyFn(elements) {
+      let value;
+      if (elements.length > 1) {
+        let fn = elements[0];
+        let args = elements.slice(1);
+        try {
+          if (fn in Scheme) {
+            value = Scheme[fn](...args);
+          } else throw `Unknown function '${fn}'`;
+        } catch(e) {
+          console.error(e);
+        }
+      } else {
+        value = elements;
+      }
+      return value;
+    }
+
+    try {
+      let elements = Scheme.#parse(str);
+      if (elements) {
+        let args = [];
+        for (let el of elements) {
+          let newArg = (el instanceof Array) ? applyFn(el) : el;
+          args.push(newArg);
+        }
+        return applyFn(args);
+      }
+    } catch(e) {
+      console.error(e);
+    }
+  }
 }
 
-// let input = "(cons 1 2)";
-let input = "(cons (cons 1 2) 3)";
-console.log(Scheme.eval(input));
+function test(fn, expr) {
+  let result = fn.call(null, expr);
+  console.log(`${fn.name} ${expr} => ${result}`);
+  return result;
+}
 
-// let outlines = sexpr(input);
-// console.log(outlines);
+let ls = Scheme.list(1, 2, 3);
 
-// let input = "list 1 2 3";
-// console.log(getArgs(input));
-// 
-// input = "(list 1) 2 3";
-// console.log(getArgs(input));
-// 
-// let input = "(list 1 2 3)";
-// console.log(input);
-// console.log(getArgs(input));
-// 
-// input = "(list (list 1 2) 3)";
-// console.log(getArgs(input));
-// 
-// input = "(list (cons 1 2) \'() 3)";
-// console.log(input);
-// console.log(getArgs(input));
+let inputs = [
+  [Scheme.cons, ["a", "b"]],
+  [Scheme.car, ls],
+  [Scheme.cdr, ls],
+  [Scheme.reverse, ls],
+  [Scheme.list, ["a", "b", "c"]],
+  [Scheme.eval, "(cons 1 2)"],
+  [Scheme.eval, "(cons (cons 1 2) 3)"],
+  [Scheme.eval, "(list 1 2 3)"],
+  [Scheme.eval, "(cons 1 (cons 2 3))"],
+  [Scheme.eval, "(list 1 2"],
+  [Scheme.eval, "(list 0 (cons 1 2) (cons 3 (cons 4 5)))"], //TODO wrong
+  [Scheme.eval, "(list 0 (cons 1 2)"], // TODO wrong
+  [Scheme.eval, "(append (cons 1 2) (list 3 4))"]
+];
 
-//input = "(list (cons 1 (list \'a \"string\" 2) 4) '() (cons 2 3))";
-// let input = "(list (cons 1 (list \'a \"string\" 2) 4) '())";
-// console.log(input);
-// console.log(getArgs(input));
+for (let [fn, arg] of inputs) {
+  test(fn, arg);
+}
 
-
-// let pair = Scheme.cons("a", "b");
-// console.log(`pair: ${pair}`);
-//  
-// let ls = Scheme.list("a", "b", "c");
-// console.log(`ls: ${ls}`);
-// 
-// let first = Scheme.head(ls);
-// console.log(`first ls: ${first}`);
-// 
-// let rest = Scheme.tail(ls);
-// console.log(`rest ls: ${rest}`);
-// 
-// let sl = Scheme.reverse(ls);
-// console.log(`reverse ls: ${sl}`);
-// 
-// let nums = Scheme.list(1, 2, 3);
-// let newNums = Scheme.map(nums, x => x + 1);
-// console.log(`map +1 ls: ${newNums}`);
-//  
-// let newPair = Scheme.eval("(cons 1 2)");
-// console.log(`newPair: ${newPair}`);
-//  
-// let newList = Scheme.eval("(list 1 2 3)");
-// console.log(`newList: ${newList}`);
-// 
-// let nested = Scheme.eval("(cons 1 (cons 2 3))");
-// console.log(`nested ${nested}`);
-// 
-// let nested2 = Scheme.eval("(list 0 (cons 1 2) (cons 3 (cons 4 5)))");
-// console.log(`nested2 ${nested2}`);
-// 
-// // let error2 = Scheme.eval("(list 0 (cons 1 2 (cons 3 (cons 4 5)))");
-// 
-// // TODO this should produce an error
-// // let error = Scheme.eval("(list 0 (cons 1 2) (cons 3 (cons 4 5))");
-// 
-// console.log(balancedDelimSubstring("(Hello)", "(", ")"));
-// console.log(balancedDelimSubstring("(Hello (world))", "(", ")"));
-// console.log(balancedDelimSubstring("(Hello (world (!)))", "(", ")", 2));
-// console.log(balancedDelimSubstring("Hello (world (!))", "(", ")", 2));
-// console.log(balancedDelimSubstring("Hello (world", "(", ")"));
-// console.log(balancedDelimSubstring("(Hello) (world)", "(", ")"));
-// console.log(balancedDelimSubstring("(Hello (or goodbye)) (world)", "(", ")"));
-// console.log(parenString("Oh (Hello (cruel) (old (world)))"));
-// console.log(parenString("O) no!(it's (alive)"));
-// console.log(parenString("fish"));
-// console.log(parenString("()"));
+let newNums = Scheme.map(ls, x => x + 1);
+console.log(`map +1 ${ls} => ${newNums}`);
 
 
+ 
