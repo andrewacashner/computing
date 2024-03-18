184e5ab4063934fc6feea88b9ac100dfdbab10a1
Andrew A. Cashner <andrewacashner@gmail.com>
2024-03-18 11:07:35 -0400

    send JSON from database

--- web-fullstack/todo/backend/todo_backend/todo/models.py
+++ web-fullstack/todo/backend/todo_backend/todo/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django import utils
 
 # Create your models here.
 class ToDoItem(models.Model):
-    task = models.TextField("Task description")
-    deadline = models.DateTimeField()
-    isDone = models.BooleanField()
+    task = models.TextField(default="Task description")
+    deadline = models.DateTimeField(default=utils.timezone.now)
+    isDone = models.BooleanField(default=False)
+    userOrder = models.IntegerField(default=0)
 
 
--- web-fullstack/todo/backend/todo_backend/todo/views.py
+++ web-fullstack/todo/backend/todo_backend/todo/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import json
 
-# Create your views here.
+from .models import ToDoItem
+
+def todo(request):
+    items = [{"task": i.task, 
+              "deadline": f"{i.deadline}", 
+              "isDone": i.isDone, 
+              "userOrder": i.userOrder }
+             for i in ToDoItem.objects.all()]
+    items.sort(key=lambda i: i["userOrder"])
+    jsonItems = json.dumps(items)
+    response = HttpResponse(jsonItems)
+    response["Content-Type"] = "text/plain"
+    return response
