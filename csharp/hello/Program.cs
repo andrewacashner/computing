1f23318fc5966dd4036d2fc826adaff87231e14c
Andrew A. Cashner <andrewacashner@gmail.com>
2024-05-23 10:03:33 -0400

    first c# program

---
+++ csharp/hello/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        if (args.Length != 2) {
+            Console.WriteLine("Usage: hello NAME");
+            Environment.ExitCode = 1;
+        } else {
+            string name = args[1];
+            Console.WriteLine(Greeting(name));
+        }
+    }
+    static string Greeting(string name)
+    {
+        return $"Hello, {name}!";
+    }
+}
