7fdcd9c2bf88de02b418e0a08782603c2ef94079
Andrew A. Cashner <andrewacashner@gmail.com>
2024-05-24 17:01:25 -0400

    C# interval calculator: accidental, pitch, interval classes

--- csharp/interval/Program.cs
+++ csharp/interval/Program.cs
@@ -9,13 +9,29 @@ internal class Program
 
         string note1, note2;
         ReadInput(args, out note1, out note2);
-
-        Pitch pitch1 = new(note1);
-        Pitch pitch2 = new(note2);
-
-        // Calculate interval
         Console.WriteLine($"Input: {note1} to {note2}");
-        Console.WriteLine($"Output: {pitch1} to {pitch2}");
+
+        Pitch? pitch1, pitch2;
+        pitch1 = pitch2 = null;
+
+        try
+        {
+            pitch1 = new Pitch(note1);
+            pitch2 = new Pitch(note2);
+            Console.WriteLine($"Output: {pitch1} to {pitch2}");
+        }
+        catch(ArgumentException ex)
+        {
+            string error = $"Could not create Pitch: {ex.Message}";
+            Console.Error.WriteLine(error);
+            Environment.Exit(1);
+        }
+
+        if (pitch1 != null && pitch2 != null)
+        {
+            Interval interval = new(pitch1, pitch2);
+            Console.WriteLine($"{interval}");
+        }
     }
 
     static void ExitIfInputInvalid(string[] args)
@@ -41,12 +57,48 @@ internal class Program
 
 namespace Musarithmetic
 {
+    
+    
+    public class Accidental
+    {
+        public enum Accid { DOUBLE_FLAT, FLAT, NATURAL, SHARP, DOUBLE_SHARP }
+
+        Accid accid = Accid.NATURAL;
+
+        public Accidental(Accid a) => accid = a;
+
+        public Accidental(string input)
+        {
+            accid = input switch 
+            {
+                "bb" => Accid.DOUBLE_FLAT,
+                "b"  => Accid.FLAT,
+                ""   => Accid.NATURAL,
+                "#"  => Accid.SHARP,
+                "##" => Accid.DOUBLE_SHARP,
+                _    => throw new ArgumentException($"""
+                        Unrecognized accidental input '{input}'
+                            (Acceptable inputs: 'bb', 'b', '#', '##', or nothing if natural)
+                        """)
+            };
+
+        }
+
+        public override string ToString() 
+        {
+            string[] outputs = ["𝄫", "♭", "♮", "♯", "𝄪"];
+            return outputs[(int)accid];
+        }
+
+        public int Adjustment() => (int)accid - 2;
+    }
+
     public class Pitch
     {
         char pname;
-        string accid; // replace with enum or class?
+        Accidental accid;
 
-        public Pitch(char pname, string accid)
+        public Pitch(char pname, Accidental accid)
         {
             this.pname = pname;
             this.accid = accid;
@@ -54,13 +106,46 @@ namespace Musarithmetic
 
         public Pitch(string rawInput)
         {
-            this.pname = Char.ToUpper(rawInput[0]);
-            this.accid = rawInput[1..];
+            char letter = Char.ToUpper(rawInput[0]);
+            if ("ABCDEFG".Contains(letter))
+                pname = letter;
+            else
+                throw new ArgumentException($"Bad Pitch input '{rawInput}'" +
+                        " (First character must be a letter A-G)");
+
+            try
+            {
+                string accidStr = rawInput[1..];
+                accid = new Accidental(accidStr);
+            }
+            catch { throw; }
         }
 
         public override string ToString()
         {
             return $"{this.pname}{this.accid}";
         }
+
+        public int DiatonicValue()
+        {
+            return "CDEFGAB".IndexOf(pname);
+        }
     }
+
+    public class Interval
+    {
+        // quality
+        int degree;
+
+        public Interval(Pitch p1, Pitch p2)
+        {
+            degree = p2.DiatonicValue() - p1.DiatonicValue();
+        }
+
+        public override string ToString()
+        {
+            return $"{degree + 1}";
+        }
+    }
+
 }
