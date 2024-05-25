f4839948b521e8b9e7b55c810b6c19bada9ae523
Andrew A. Cashner <andrewacashner@gmail.com>
2024-05-25 00:24:53 -0400

    C# interval calculator complete

--- csharp/interval/Program.cs
+++ csharp/interval/Program.cs
@@ -9,7 +9,6 @@ internal class Program
 
         string note1, note2;
         ReadInput(args, out note1, out note2);
-        Console.WriteLine($"Input: {note1} to {note2}");
 
         Pitch? pitch1, pitch2;
         pitch1 = pitch2 = null;
@@ -18,7 +17,6 @@ internal class Program
         {
             pitch1 = new Pitch(note1);
             pitch2 = new Pitch(note2);
-            Console.WriteLine($"Output: {pitch1} to {pitch2}");
         }
         catch(ArgumentException ex)
         {
@@ -29,8 +27,16 @@ internal class Program
 
         if (pitch1 != null && pitch2 != null)
         {
-            Interval interval = new(pitch1, pitch2);
-            Console.WriteLine($"{interval}");
+            try 
+            {
+                Interval interval = new(pitch1, pitch2);
+                Console.WriteLine($"{pitch1} to {pitch2} = {interval}");
+            }
+            catch(ArgumentException ex)
+            {
+                Console.Error.WriteLine($"Problem creating interval: {ex.Message}");
+                Environment.Exit(1);
+            }
         }
     }
 
@@ -57,19 +63,15 @@ internal class Program
 
 namespace Musarithmetic
 {
-    
-    
     public class Accidental
     {
-        enum Accid { DOUBLE_FLAT, FLAT, NATURAL, SHARP, DOUBLE_SHARP }
+        enum Accid { DOUBLE_FLAT, FLAT, NATURAL, SHARP, DOUBLE_SHARP };
 
         Accid accid = Accid.NATURAL;
 
-        // public Accidental(Accid a) => accid = a;
-
-        public Accidental(string input)
+        public Accidental(string accidStr)
         {
-            accid = input switch 
+            accid = accidStr switch 
             {
                 "bb" => Accid.DOUBLE_FLAT,
                 "b"  => Accid.FLAT,
@@ -77,7 +79,7 @@ namespace Musarithmetic
                 "#"  => Accid.SHARP,
                 "##" => Accid.DOUBLE_SHARP,
                 _    => throw new ArgumentException($"""
-                        Unrecognized accidental input '{input}'
+                        Unrecognized accidental input '{accidStr}'
                             (Acceptable inputs: 'bb', 'b', '#', '##', or nothing if natural)
                         """)
             };
@@ -93,30 +95,69 @@ namespace Musarithmetic
         public int Adjustment() => (int)accid - 2;
     }
 
-    public class Pitch
+    public class PitchName
     {
-        char pname;
-        Accidental accid;
+        enum Pname { PC_C, PC_D, PC_E, PC_F, PC_G, PC_A, PC_B };
 
-        public Pitch(char pname, Accidental accid)
+        Pname pname;
+
+        public PitchName(int offset)
         {
-            this.pname = pname;
-            this.accid = accid;
+            pname = (Pname)offset;
         }
 
-        public Pitch(string rawInput)
+        public PitchName(char pnameChar)
         {
-            char letter = Char.ToUpper(rawInput[0]);
-            if ("ABCDEFG".Contains(letter))
-                pname = letter;
-            else
-                throw new ArgumentException($"Bad Pitch input '{rawInput}'" +
-                        " (First character must be a letter A-G)");
+            pnameChar = Char.ToUpper(pnameChar);
 
+            pname = pnameChar switch
+            {
+                'C' => Pname.PC_C,
+                'D' => Pname.PC_D,
+                'E' => Pname.PC_E,
+                'F' => Pname.PC_F,
+                'G' => Pname.PC_G,
+                'A' => Pname.PC_A,
+                'B' => Pname.PC_B,
+                _   => throw new ArgumentException($"""
+                        Unrecognized pitch name '{pnameChar}'
+                            (Must be a letter A-G)
+                        """)
+            };
+        }
+
+        public override string ToString()
+        {
+            string[] outputs = ["C", "D", "E", "F", "G", "A", "B"];
+            return outputs[(int)pname];
+        }
+
+        public int DiatonicOffset() => (int)pname;
+
+        public int ChromaticOffset()
+        {
+            int[] offsets = [0, 2, 4, 5, 7, 9, 11];
+            return offsets[(int)pname];
+        }
+
+        public static int ChromaticOffset(int degree)
+        {
+            return new PitchName(degree).ChromaticOffset();
+        }
+    }
+
+
+    public class Pitch
+    {
+        PitchName pname;
+        Accidental accid;
+
+        public Pitch(string pitchStr)
+        {
             try
             {
-                string accidStr = rawInput[1..];
-                accid = new Accidental(accidStr);
+                pname = new PitchName(pitchStr[0]);
+                accid = new Accidental(pitchStr[1..]);
             }
             catch { throw; }
         }
@@ -126,31 +167,111 @@ namespace Musarithmetic
             return $"{this.pname}{this.accid}";
         }
 
-        public int DiatonicValue()
+        public int DiatonicValue() => pname.DiatonicOffset();
+        
+        public int ChromaticValue()
         {
-            return "CDEFGAB".IndexOf(pname);
+            int offset = pname.ChromaticOffset();
+            int adjustment = accid.Adjustment();
+            return offset + adjustment;
         }
+
+    }
+
+    public class Quality
+    {
+        enum Qual { DIMINISHED, MINOR, MAJOR, PERFECT, AUGMENTED };
+
+        Qual quality;
+        
+        // 0-indexed intervals (= one-indexed P1, P4, P5)
+        int[] perfectIntervals = [0, 3, 4];
+
+        public Quality(int degree, int adjustment)
+        {
+            quality = adjustment switch 
+            {
+                -2 => Qual.DIMINISHED,
+                -1 => perfectIntervals.Contains(degree) 
+                        ? Qual.DIMINISHED : Qual.MINOR,
+                 0 => perfectIntervals.Contains(degree)
+                        ? Qual.PERFECT : Qual.MAJOR,
+                 1 => Qual.AUGMENTED,
+                _  => throw new ArgumentException($"Interval out of range")
+            };
+        }
+/*
+        public int Value()
+        {
+            int qualityValue = qual switch
+            {
+                Qual.DIMINISHED => perfectIntervals.Contains(interval) ? -1 : -2,
+                Qual.MINOR      => -1,
+                Qual.MAJOR      => 0,
+                Qual.PERFECT    => 0,
+                Qual.AUGMENTED  => 1,
+                _          => throw new ArgumentException($"Quality Value must be a Quality enum instance")
+            };
+            return qualityValue;
+        }
+*/
+        public override string ToString()
+        {
+            string[] outputs = ["d", "m", "M", "P", "A"];
+            return outputs[(int)quality];
+        }
+
+
     }
 
     public class Interval
     {
-        // quality
+        Quality quality;
         int degree;
 
         public Interval(Pitch p1, Pitch p2)
         {
+            degree = DiatonicInterval(p1, p2);
+            
+            int defaultInterval = PitchName.ChromaticOffset(degree);
+            int inflectedInterval = ChromaticInterval(p1, p2);
+            int adjustment = inflectedInterval - defaultInterval;
+
+            try 
+            {
+                quality = new Quality(degree, adjustment);
+            }
+            catch { throw; }
+        }
+
+
+        static int loopDiff(int n, int m, int max)
+        {
+            if (m < n)
+                m += max;
+
+            return m - n;
+        }
+
+        // TODO replace this and next with generics?
+        static int DiatonicInterval(Pitch p1, Pitch p2)
+        {
             int val1 = p1.DiatonicValue();
             int val2 = p2.DiatonicValue();
-
-            if (val2 < val1)
-                val2 += 7;
-
-            degree = val2 - val1;
+            return loopDiff(val1, val2, 7);
         }
+        
+        static int ChromaticInterval(Pitch p1, Pitch p2)
+        {
+            int val1 = p1.ChromaticValue();
+            int val2 = p2.ChromaticValue();
+            return loopDiff(val1, val2, 12);
+        }
+
 
         public override string ToString()
         {
-            return $"{degree + 1}";
+            return $"{quality}{degree + 1}";
         }
     }
 
