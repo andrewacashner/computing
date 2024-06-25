6c3d24132533d07ca8de003be173ffa1939c7ff0
Andrew A. Cashner <andrewacashner@gmail.com>
2024-06-25 10:06:17 -0400

    file input or stdin

---
+++ csharp/delegate/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string path = args.Length > 0 ? args.ElementAt(0) : "";
+        string input = "";
+
+        if (path == "")
+        {
+            string? thisLine;
+            while ((thisLine = Console.ReadLine()) != null)
+            {
+                input += thisLine + "\n";
+            }
+        }
+        else
+        {
+            input = File.ReadAllText(path);
+        }
+
+        Console.WriteLine(input);
+    }
+}
