5213cde9aa3cfdc8bd4886b6e1138f6574f00ad0
Andrew A. Cashner <andrewacashner@gmail.com>
2024-09-10 10:05:16 -0400

    mcc csc101 assts

---
+++ java/Goodbye.java
@@ -0,0 +1,26 @@
+/* CSC101, Lab 1
+ * Andrew Cashner, acashner@student.monroecc.edu
+ * 2024/09/05
+ */
+ 
+class Goodbye {
+
+   public static void main(String[] args) {
+      System.out.print("\nMy favorite numbers are:\n");
+      System.out.print("17   35\n");
+      System.out.print("55   75\n");
+      System.out.println();
+      
+      String name = "Andrew"; // my name
+      System.out.println("My name is \"" + name + "\" and this is my first program!");
+      System.out.print("\n");
+      
+      System.out.print("I play chess.\t");
+      System.out.println("I do not play chess well.");
+      
+      System.out.println("I play chess.\tI do not play chess well.");
+           
+      System.out.println("A backslash character ( \\ ) is not used in proper English");
+      System.out.println("\nGoodbye");      
+   }
+}
---
+++ java/Hello.java
@@ -0,0 +1,23 @@
+/* CSC101 (Fall 2024), Lab 1
+ * Andrew Cashner, acashner@student.monroecc.edu
+ * 2024/09/05
+ */
+ 
+ // Start of code
+ // Hello World example
+ 
+ class Hello {
+    
+    public static void main( String [] args) { // start of main
+    
+       String message; //message is declared as a String variable
+       
+       message = "Hello World";//then assigned a value
+       
+       System.out.println(message);
+       System.out.println("This is a String Literal");
+    
+    } // end of main method
+ }// end of class Hello
+ 
+ //end of code
