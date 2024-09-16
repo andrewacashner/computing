06b177d87353871b4c6912d7c89ad7694d6ea63b
Andrew A. Cashner <andrewacashner@gmail.com>
2024-09-16 13:11:22 -0400

    fix rounding error when casting by explicitly calling Math.round()

--- java/csc101/project1/Change.java
+++ java/csc101/project1/Change.java
@@ -1,70 +1,68 @@
 /* Change
  * CSC101 Project 1
- * Andrew Cashner, 2024/09/12
+ * Andrew Cashner, 2024/09/16
  */
 
 import java.util.Scanner;
 
 class Change {
-    
-    static final int DOLLAR = 100;
-    static final int QUARTER = 25;
-    static final int DIME = 10;
-    static final int NICKEL = 5;
-    static final int PENNY = 1;
 
-    static int change(int total, int value) {
-        return (int)total / value;
-    }
+   static final int DOLLAR  = 100;
+   static final int QUARTER =  25;
+   static final int DIME    =  10;
+   static final int NICKEL  =   5;
+   static final int PENNY   =   1;
 
-    static int remainder(int total, int value, int quantity) {
-        return total - (value * quantity);
-    }
+   static int change(int total, int value) {
+      return total / value;
+   }
 
-    static String changeTableRow(String label, int amount) {
-        return String.format("  %-10s%5d\n", label, amount);
-    }
+   static int remainder(int total, int value, int quantity) {
+      return total - (value * quantity);
+   }
 
-    public static void main(String[] args) {
-        Scanner kbScan = new Scanner(System.in);
+   static String changeRow(String label, int amount) {
+      return String.format("  %-10s%5d\n", label, amount);
+   }
 
-        System.out.println("CHANGE CALCULATOR");
-        System.out.print("Enter price of product: $");
-        double price = kbScan.nextDouble();
+   public static void main(String[] args) {
+      Scanner kbScan = new Scanner(System.in);
 
-        System.out.print("Enter amount tendered:  $");
-        double payment = kbScan.nextDouble();
+      System.out.println("CHANGE CALCULATOR");
+      System.out.print("Enter price of product: $");
+      double price = kbScan.nextDouble();
 
-        // Assuming payment is more than price
-        double changeTotal = payment - price;
+      System.out.print("Enter amount tendered:  $");
+      double payment = kbScan.nextDouble();
 
-        // TODO off by one cent ?
-        int changeLeft = (int)(changeTotal * 100.0);
-        
-        int changeDollars = change(changeLeft, DOLLAR);
-        changeLeft = remainder(changeLeft, DOLLAR, changeDollars);
+      // Assuming payment is more than price
+      double changeTotal = payment - price;
 
-        int changeQuarters = change(changeLeft, QUARTER);
-        changeLeft = remainder(changeLeft, QUARTER, changeQuarters);
+      // Round to avoid inaccuracy in conversion to int
+      int changeLeft = (int)Math.round(changeTotal * 100);
 
-        int changeDimes = change(changeLeft, DIME);
-        changeLeft = remainder(changeLeft, DIME, changeDimes);
+      int changeDollars = change(changeLeft, DOLLAR);
+      changeLeft = remainder(changeLeft, DOLLAR, changeDollars);
 
-        int changeNickels = change(changeLeft, NICKEL);
-        changeLeft = remainder(changeLeft, NICKEL, changeNickels);
-        
-        int changePennies = change(changeLeft, PENNY);
+      int changeQuarters = change(changeLeft, QUARTER);
+      changeLeft = remainder(changeLeft, QUARTER, changeQuarters);
 
-        String table = 
-            changeTableRow("Dollars", changeDollars) +
-            changeTableRow("Quarters", changeQuarters) + 
-            changeTableRow("Dimes", changeDimes) + 
-            changeTableRow("Nickels", changeNickels) +
-            changeTableRow("Pennies", changePennies);
+      int changeDimes = change(changeLeft, DIME);
+      changeLeft = remainder(changeLeft, DIME, changeDimes);
 
-        System.out.printf("Total change: $%.2f\n\n", changeTotal);
-        System.out.print(table);
+      int changeNickels = change(changeLeft, NICKEL);
+      changeLeft = remainder(changeLeft, NICKEL, changeNickels);
 
-    }
+      int changePennies = change(changeLeft, PENNY);
 
+      String table = 
+         changeRow("Dollars", changeDollars) +
+         changeRow("Quarters", changeQuarters) + 
+         changeRow("Dimes", changeDimes) + 
+         changeRow("Nickels", changeNickels) +
+         changeRow("Pennies", changePennies);
+
+      System.out.printf("Total change: $%.2f\n\n", changeTotal);
+      System.out.print(table);
+   }
 }
--- java/csc101/project1/StringStuff.java
+++ java/csc101/project1/StringStuff.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
 import java.util.Random;
 
 class StringStuff {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Random randGen = new Random();
