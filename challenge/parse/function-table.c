da89711cc4aaee9e096605eb7d0346b0c2c52bcd
Andrew A. Cashner <andrewacashner@gmail.com>
2024-10-21 08:35:54 -0400

    working calculator: read sexpr from stdin, find function in table and check arguments, calculate results

--- challenge/parse/function-table.c
+++ challenge/parse/function-table.c
@@ -1,142 +1,199 @@
 /* Call a function by name from a table of functions
  * Andrew Cashner, 2024/10/18
+ *
+ * TODO
+ * not just ints?
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
-#define MAX_ARGS 7
-#define MAX_CHAR 20
+#define MAX_CHAR_ATOM 20
+#define MAX_CHAR_INPUT 80
+#define MAX_ARGS 10
+#define INF MAX_ARGS
+#define FUNCTION_COUNT 6
 
-#define FUNCTION_COUNT 4
+typedef double (*generic_function_ptr)(int, double[]);
 
-int add(int, int);
-int subtract(int, int);
-int multiply(int, int);
-int divide(int, int);
+double apply(double (*fn)(double, double), int argc, double argv[]);
+double add(int, double[]);
+double subtract(int, double[]);
+double multiply(int, double[]);
+double divide(int, double[]);
+double absolute_value(int, double[]);
+double exponent(int, double[]);
+
 
 typedef struct Function_sig *Function_sig_ptr;
-
 typedef struct Function_sig {
-    char name[MAX_CHAR];
-    int (*function)(int, int);
+    char name[MAX_CHAR_ATOM];
+    char symbol[MAX_CHAR_ATOM];
+    generic_function_ptr function;
     int required_args;
 } Function_sig;
 
-Function_sig Function_sig_create(char*, int (*fn)(int, int), int);
-Function_sig_ptr lookup_function(Function_sig_ptr table[], char*);
+Function_sig_ptr lookup_function(Function_sig[], char*);
 
-char *function_names[] = {
-    "+",
-    "-",
-    "*",
-    "/",
-};
-
-int (*function_methods)(int, int)[] = {
-    add,
-    subtract,
-    multiply,
-    divide
-}
-
-int function_required_args[] = {
-    2,
-    2, 
-    2, 
-    2,
-};
-
-    
-int main(int argc, char *argv[]) {
-    // Or can you initialize above with struct literals?
-    Function_sig_ptr functions[FUNCTION_COUNT];
-
-    for (int i = 0; i < FUNCTION_COUNT; ++i) {
-        Function_sig_ptr this_function = Function_sig_create(
-               function_names[i],
-               function_methods[i],
-               function_required_args[i]);
-        Function_sig_functions[i] = this_function;
+Function_sig _function_table[] = {
+    {   .name = "add",
+        .symbol = "+",
+        .function = (generic_function_ptr)add,
+        .required_args = MAX_ARGS
+    },
+    {
+        .name = "subtract",
+        .symbol = "-",
+        .function = (generic_function_ptr)subtract,
+        .required_args = MAX_ARGS
+    },
+    {
+        .name = "multiply",
+        .symbol = "*",
+        .function = (generic_function_ptr)multiply,
+        .required_args = MAX_ARGS
+    },
+    {
+        .name = "divide",
+        .symbol = "/",
+        .function = (generic_function_ptr)divide,
+        .required_args = MAX_ARGS
+    },
+    {
+        .name = "absolute value",
+        .symbol = "abs",
+        .function = (generic_function_ptr)absolute_value,
+        .required_args = 1
+    },
+    {
+        .name = "exponent",
+        .symbol = "expt",
+        .function = (generic_function_ptr)exponent,
+        .required_args = 2
     }
+};
 
-    // Read arguments into variables
-    if (argc <= 1) {
-        printf("Usage: function-table FUNCTION ARG1 ARG2 ...\n");
+int main(void) {
+    char buffer[MAX_CHAR_INPUT];
+    fgets(buffer, sizeof(buffer), stdin);
+    buffer[strlen(buffer) - 1] = '\0';
+    char *input = buffer;
+
+    if (input[0] == '(' && input[strlen(input) - 1] == ')') {
+        input[strlen(input) - 1] = '\0';
+        ++input;
+    } else {
+        fprintf(stderr, "Expression must be enclosed in parentheses\n");
         exit(EXIT_FAILURE);
     }
+  
+    double args[MAX_ARGS];
+    int arg_count = -1;
+    char *input_function_name;
 
-    char *input_function_name = argv[1];
-    char *args[MAX_ARGS];
-    int arg_count;
+    for (char *token = strtok(input, " ");
+            token;
+            token = strtok(NULL, " ")) {
 
-    for (int i = 2, j = 0; 
-            i < argc && j < MAX_ARGS - 2; 
-            ++i, ++j) {
-
-        args[j] = argv[i];
+        if (arg_count < 0) {
+            input_function_name = token;
+        } else {
+            args[arg_count] = atof(token);
+        }
         ++arg_count;
     }
 
     // Parse them
     // Compare to fn table
-    // TODO but there will be variable numbers of arguments
-    // also not just ints
-    Function_sig_ptr match_sig = lookup_function(functions, input_function_name);
-    if (!found) {
-        fprintf(stderr, "Unrecognized function %s\n", name);
+    Function_sig_ptr match_sig = 
+        lookup_function(_function_table, input_function_name);
+
+    if (!match_sig) {
+        fprintf(stderr, "Unrecognized function %s\n", input_function_name);
         exit(EXIT_FAILURE);
     }
-    int (*fn)(int, int) = match_sig->function;
+    generic_function_ptr fn = match_sig->function;
+    char *name = match_sig->name;
     int required_args = match_sig->required_args;
 
-    // Apply function
-    if (arg_count != required_args) {
-        fprintf(stderr, "Wrong number of arguments to function %s (%d required, %d supplied)\n", 
-                input_function, required_args, arg_count);
+    // Test arguments
+    if (required_args != INF && arg_count != required_args) {
+        fprintf(stderr, "Wrong number of arguments to function %s (%d required, %d supplied)\n", name, required_args, arg_count);
         exit(EXIT_FAILURE);
     }
 
-    int result = (*fn)(args[0], args[1]);
-    printf("=> %d\n", result);
+    printf("Apply function %s to %d arguments: ", name, required_args);
 
-    for (int i = 0; i < FUNCTION_COUNT; ++i) {
-        free(functions[i]);
+    for (int i = 0; i < arg_count; ++i) {
+        printf("%f ", args[i]);
     }
+    printf("\n");
+
+    // Calculate and report result
+    double result = (*fn)(arg_count, args);
+    printf("=> %f\n", result);
 
     return 0;
 }
 
-int add(int a, int b) {
-    return a + b;
+double apply(double (*fn)(double, double), int argc, double argv[]) {
+    double result = argv[0];
+    for (int i = 1; i < argc; ++i) {
+        result = fn(result, argv[i]);
+    }
+    return result;
 }
 
-int subtract(int a, int b) {
-    return a - b;
+double do_add(double n, double m) {
+    return n + m;
 }
 
-int multiply(int a, int b) {
-    return a * b;
+double add(int argc, double argv[]) {
+    return apply(do_add, argc, argv);
 }
 
-int divide(int a, int b) {
-    return a / b;
+double do_subtract(double n, double m) {
+    return n - m;
 }
 
-Function_sig Function_sig_create(char *name, 
-        int (*fn)(int, int), int args) {
-    Function_sig this = malloc(sizeof(Function_sig));
-    strcpy(this->name, name);
-    this->function = fn;
-    this->required_args = args;
+double subtract(int argc, double argv[]) {
+    return apply(do_subtract, argc, argv);
 }
 
-Function_sig_ptr lookup_function(Function_sig_ptr table[FUNCTION_COUNT], char *name) {
+double do_multiply(double n, double m) {
+    return n * m;
+}
+
+double multiply(int argc, double argv[]) {
+    return apply(do_multiply, argc, argv);
+}
+
+double do_divide(double n, double m) {
+    return n / m;
+}
+
+double divide(int argc, double argv[]) {
+    return apply(do_divide, argc, argv);
+}
+
+
+double absolute_value(int argc, double argv[]) {
+    return abs(argv[0]);
+}
+
+double exponent(int argc, double argv[]) {
+    return pow(argv[0], argv[1]);
+}
+
+Function_sig_ptr lookup_function(Function_sig table[], char *symbol) {
+
     Function_sig_ptr found = NULL;
 
     for (int i = 0; i < FUNCTION_COUNT; ++i) {
-        if (strcmp(table[i]->name, name) == 0) {
-            found = table[i];
+        if (strcmp(table[i].symbol, symbol) == 0) {
+            found = &table[i];
             break;
         }
     }
