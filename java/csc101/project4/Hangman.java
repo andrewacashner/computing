4dd062075e366915499f5f74766a82519755a2aa
Andrew A. Cashner <andrewacashner@gmail.com>
2024-10-28 08:40:59 -0400

    start of hangman game

--- java/csc101/project4/Hangman.java
+++ java/csc101/project4/Hangman.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Hangman Game
  *
@@ -5,14 +7,135 @@
  * @version 2024/10/25 (CSC 101, Project 4)
  */
 public class Hangman {
-    public static void main(String[] args) {
-        // Get answer from user
-        // Get number of wrong guesses allowed from user
-        // Game loop till too many wrong guesses:
-        //      Show prompt
-        //      Evaluate guess
-        //      Report result of guess
-        //      Revise prompt, update score, list of guesses
-        // Report result of game
-    }
+   /** 
+    * Ask user for secret word and number of allowed wrong guesses.
+    * In the main game loop, set up the game board and prompt the user to
+    * guess, check the answer, and update the display with the result, until
+    * the player guesses correctly or has too many wrong guesses.
+    *
+    * @param args (Unused) Command-line arguments
+    */
+   public static void main(String[] args) {
+      // Get answer from user
+      // Get number of wrong guesses allowed from user
+      Scanner kbScan = new Scanner(System.in);
+
+      System.out.println("========== HANGMAN ==========");
+      System.out.print("Enter the secret word (or phrase): ");
+      String answer = kbScan.nextLine();
+
+      System.out.print("Enter the maximum number of wrong guesses: ");
+      int maxWrongGuesses = kbScan.nextInt();
+
+      int wrongGuesses = 0;
+      String lettersGuessed = new String();
+
+      String input = new String();
+
+      // Game loop till too many wrong guesses:
+      while (!input.equals("!") && wrongGuesses < maxWrongGuesses) {
+         // Show prompt
+         String display = display(answer, lettersGuessed, 
+               wrongGuesses, maxWrongGuesses);
+         System.out.print(display);
+         System.out.print("Guess a letter (or ! to quit): ");
+         input = kbScan.next();
+         char guess = input.charAt(0);
+         if (guess == '!') {
+            continue;
+         }
+
+         // Evaluate guess
+         lettersGuessed = recordGuess(lettersGuessed, guess);
+         if (!isCorrectGuess(guess, answer)) {
+            ++wrongGuesses;
+         }
+
+         // Report result of guess
+         // Revise prompt, update score, list of guesses
+
+      }
+
+      // Report result of game
+      System.out.println("Thank you for playing Hangman!");
+   }
+
+   static String recordGuess(String lettersGuessed, char guess) {
+      return lettersGuessed + Character.toString(guess);
+   }
+
+   static boolean contains(String str, char guess) {
+      return str.indexOf(guess) > 0;
+   }
+
+   static boolean isCorrectGuess(char guess, String answer) {
+      return contains(answer, guess);
+   }
+
+   static char matchOrBlank(String word, String lettersToShow, int i) {
+      final char BLANK = '_';
+      
+      char thisChar = word.charAt(i);
+      char displayChar = BLANK;
+  
+      if (lettersToShow.contains(Character.toString(thisChar))) {
+         displayChar = thisChar;
+      } 
+
+      return displayChar;
+   }
+
+   static String blanks(String word, String lettersToShow) {
+      StringBuilder blanks = new StringBuilder();
+
+      for (int i = 0; i < word.length(); ++i) {
+         char newChar = matchOrBlank(word, lettersToShow, i);
+         blanks.append(String.format("%c ", newChar));
+      }
+      return blanks.toString();
+   }
+
+//        😵
+//      💪👔🤳
+//        👖
+//       👟👟
+//
+   static String drawHangman(int wrongGuesses) {
+      final String GALLOWS = "-------¬\n";
+      final String[] HANGMAN = {
+         "|       😵\n",
+         "|    💪", 
+         "👔",
+         "🤳\n",
+         "|      👖\n",
+      };
+
+      String HANGMAN_FOOT = new String("👟");
+      StringBuilder display = new StringBuilder(GALLOWS);
+
+      for (int i = 0; i < wrongGuesses && i < HANGMAN.length; ++i) {
+         display.append(HANGMAN[i]);
+         --wrongGuesses;
+      }
+      for (int i = wrongGuesses - HANGMAN.length - 2; i > 0; --i) {
+         String nextStr = i % 2 == 0 ? "|  " : "  |\n";
+         display.append(nextStr);
+         --wrongGuesses;
+      }
+      for (int i = wrongGuesses - HANGMAN.length + 2; i > 0; --i) {
+         display.append(HANGMAN_FOOT);
+      }
+      display.append("\n");
+      return display.toString();
+   }
+     
+
+   public static String display(String answer, String lettersGuessed,
+         int wrongGuesses, int maxWrongGuesses) {
+      String blanks = blanks(answer, lettersGuessed);
+      String hangman = drawHangman(wrongGuesses);
+      String guesses = String.format("Letters guessed: %s\n", lettersGuessed.toString());
+      return String.format("%20s%20s\n%s\n", blanks, guesses, hangman);
+   }
+
 }
