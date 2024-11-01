dbe7e42a0b749be06cf8de9cb38db5ec7136f579
Andrew A. Cashner <andrewacashner@gmail.com>
2024-10-31 20:48:27 -0400

    start of password checker

--- java/csc101/project4/PasswordCheck.java
+++ java/csc101/project4/PasswordCheck.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Password checker
  *
@@ -17,5 +19,71 @@ public class PasswordCheck {
     // conditions it fails.
     // Loop to test multiple passwords until user exits.
     public static void main(String[] args) {
+        Scanner kbScan = new Scanner(System.in);
+
+        System.out.println("PASSWORD CHECKER");
+        String input = new String();
+
+        while (input != "q") {
+            System.out.print("Enter a password to test (or 'q' to quit): ");
+            String password = kbScan.nextLine();
+            if (isValid(password)) {
+                // say so
+            } else {
+                // say so
+                // report failure points
+            }
+        }
+
+        System.out.println("Thank you for using the Password Checker.");
+    }
+
+    static boolean validContentLength(String password) {
+        int length = password.length();
+        return length >= 10 && length <= 16;
+    }
+
+    static boolean validContentCapitals(String password) {
+        int capitalCount = 0;
+        for (int i = 0; i < password.length(); ++i) {
+            if (Character.isUpperCase(password.charAt(i))) {
+                ++capitalCount;
+            }
+        }
+        return capitalCount;
+    }
+
+    static boolean contains(String set, char c) {
+        boolean found = false;
+        for (int i = 0; i < set.length() && !found; ++i) {
+            found = set.charAt(i) == c;
+        }
+        return found;
+    }
+
+    static boolean containsAtLeast(String str, String matchSet, 
+            int minMatches) {
+
+        int foundMatches = 0;
+        for (int i = 0; i < str.length(); ++i) {
+            if (contains(matchSet, str.charAt(i))) {
+                ++foundMatches;
+            }
+        }
+        return foundMatches >= minMatches;
+    }
+
+    static boolean validContentSymbols(String password) {
+        String symbols = new String("!#$%^&*<>?");
+        return containsAtLeast(password, symbols, 1);
+    }
+
+    static boolean validContentDigits(String password) {
+        String digits = "0123456789";
+        return containsAtLeast(digits, 1);
+    }
+
+    static boolean validContentSpaces(String password) {
+        return password.indexOf(' ') == -1;
     }
 }
