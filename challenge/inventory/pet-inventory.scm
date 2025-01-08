630f475b83e5da22487dec0dcdd9ed383915daa5
Andrew A. Cashner <andrewacashner@gmail.com>
2025-01-08 16:15:17 -0500

    different approach in scheme

--- challenge/inventory/pet-inventory.scm
+++ challenge/inventory/pet-inventory.scm
@@ -1,27 +1,46 @@
-(use-modules
-  (srfi srfi-1))
+(define new-pet
+  (lambda (type)
+    (lambda (name)
+      (cons type name))))
 
-(define any-pet
-  (lambda (pet-name pet-says)
-    `((name ,pet-name)
-      (says ,pet-says))))
+(define pet (new-pet 'default))
+(define dog (new-pet 'dog))
+(define cat (new-pet 'cat))
 
-(define pet-type
-  (lambda (says)
-    (lambda (name) (any-pet name says))))
+(define pet-type car)
+(define pet-name cdr)
 
-(define pet (pet-type "?"))
-(define dog (pet-type "Woof"))
-(define cat (pet-type "Meow"))
+(define pet-sounds
+  '((dog . "Woof")
+    (cat . "Meow")
+    (default . "?")))
 
 (define pet-says
   (lambda (pet)
-    (car (assq-ref pet 'says)))) 
-; car is necessary because of quasiquote situation above
+    (assq-ref pet-sounds (pet-type pet))))
 
-(define pet-name
-  (lambda (pet)
-    (car (assq-ref pet 'name))))
+;; Another approach
+; (define any-pet
+;   (lambda (pet-name pet-says)
+;     `((name ,pet-name)
+;       (says ,pet-says))))
+; 
+; (define pet-type
+;   (lambda (says)
+;     (lambda (name) (any-pet name says))))
+; 
+; (define pet (pet-type "?"))
+; (define dog (pet-type "Woof"))
+; (define cat (pet-type "Meow"))
+; 
+; (define pet-says
+;   (lambda (pet)
+;     (car (assq-ref pet 'says)))) 
+; ; car is necessary because of quasiquote situation above
+; 
+; (define pet-name
+;   (lambda (pet)
+;     (car (assq-ref pet 'name))))
 
 (define pet-greeting
   (lambda (pet)
@@ -31,7 +50,7 @@
        [cat-names '("Fluffy" "Tiny" "Madame Clawe")]
        [dogs (map dog dog-names)]
        [cats (map cat cat-names)]
-       [pets (concatenate (list dogs cats))]
+       [pets (apply append (list dogs cats))]
        [pets (cons (pet "Baron von Fiend") pets)]) ; shuffle
   (display (string-join (map pet-greeting pets) "\n"))
   (newline))
