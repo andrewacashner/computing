c5afe7ccc55a6fda2e55639fad758f911f95a9a4
Andrew A. Cashner <andrewacashner@gmail.com>
2025-08-05 08:19:42 -0400

    play with templates: contains, index_of functions; some vectors instead of lists

--- c++/grocery.cpp
+++ c++/grocery.cpp
@@ -6,22 +6,22 @@
  * Modify the list at the command line.
  *
  *      grocery --list "$HOME"/.grocery/grocery.txt
- *      grocery --restart 
+ *      grocery --reset
  *      grocery --add milk
  *      grocery --add eggs --add flour
  *      grocery --remove margarine
  *      grocery --print 
  *      grocery --list party.txt --add seltzer --add cupcakes --print
+ *      grocery --add chicken --list birthday.txt --add 'chocolate chips'
  */
 
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <list>
 #include <vector>
 #include <algorithm>
 #include <iterator>
-#include <functional>
+#include <list>
 
 using std::string;
 
@@ -36,16 +36,7 @@ class Command {
         string arg { "" };
 
     public:
-        Command(string word, string arg) :arg(arg) 
-        { 
-            auto match = find(words.begin(), words.end(), word);
-
-            if (match != words.end()) {
-                action = (Action) std::distance(words.begin(), match);
-            } 
-            else throw std::invalid_argument{ "Unrecognized action '" + word + "'" };
-        }
-
+        Command(string word, string arg);
         Command(string action_str) { Command(action_str, ""); }
 
         Action get_action() { return action; }
@@ -59,108 +50,24 @@ class GroceryList {
         std::list<string> items;
         string source_file { "grocery_list.txt" };
 
-        bool contains(string item) {
-            return (std::find(items.begin(), items.end(), item) != items.end());
-        }
-
-        void set_list(string filename)
-        {
-            source_file = filename;
-            load();
-        }
-
-        void reset() { items.clear(); }
-
-        void add(string item)
-        {
-            if (contains(item)) {
-                std::cout << "Item '" << item << "' already on list\n";
-            }
-            else {
-                items.push_back(item);
-                save();
-            }
-        }
-
-        void remove(string item)
-        {
-            if (!contains(item)) {
-                std::cout << "Item '" << item << "' not on list; could not remove\n";
-            }
-            else {
-                items.remove(item);
-                save();
-            }
-        }
-
-        void print()
-        {
-            std::cout << "\nGrocery List\n" << "------------\n";
-            for (string item : items) {
-                std::cout << item << "\n";
-            }
-        }
+        void load();
+        void save();
+        bool contains(string);
+        void set_list(string);
+        void reset();
+        void add(string);
+        void remove(string);
+        void print();
 
     public:
         GroceryList()   { load(); }
         ~GroceryList()  { save(); }
 
-        void load()
-        {
-            items.clear();
-
-            std::fstream list_file { source_file, std::fstream::in };
-
-            string line;
-            while (std::getline(list_file, line)) {
-                items.push_back(line);
-            }
-
-            list_file.close();
-        }
-
-        void save()
-        {
-            std::fstream list_file { source_file, 
-                std::fstream::out | std::fstream::trunc };
-          
-            items.sort();
-
-            for (string item : items) {
-                list_file << item << "\n";
-            }
-            
-            list_file.close();
-        }
-
-
-        void execute(Command& cmd)
-        { 
-            switch (cmd.get_action()) {
-                case Command::Action::LIST:
-                    set_list(cmd.get_arg());
-                    break;
-
-                case Command::Action::RESET:
-                    reset();
-                    break;
-
-                case Command::Action::ADD:
-                    add(cmd.get_arg());
-                    break;
-
-                case Command::Action::REMOVE:
-                    remove(cmd.get_arg());
-                    break;
-
-                default: // Command::Action::PRINT:
-                    print();
-                    break;
-            }
-        }
+        void execute(Command& cmd);
 };
 
-std::list<Command> parse_args(char **argv);
+std::vector<Command> parse_args(char **argv);
+
 
 int main(int argc, char *argv[])
 {
@@ -172,7 +79,7 @@ int main(int argc, char *argv[])
     try {
         GroceryList grocery_list;
 
-        std::list<Command> commands = parse_args(argv);
+        std::vector<Command> commands = parse_args(argv);
         for (Command command : commands) {
             // cout << command.to_string() << "\n";
             grocery_list.execute(command);
@@ -187,9 +94,10 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-std::list<Command> parse_args(char **argv)
+
+std::vector<Command> parse_args(char **argv)
 {
-    std::list<Command> commands;
+    std::vector<Command> commands;
 
     for (int i = 1; argv[i]; ++i) {
         if (string word = argv[i]; word.starts_with("--")) {
@@ -208,5 +116,117 @@ std::list<Command> parse_args(char **argv)
     }
 
     return commands;
+}
+
+template<typename S, typename O>
+int index_of(const S& seq, const O& obj)
+{
+    auto match = std::find(seq.begin(), seq.end(), obj);
+    return (match != seq.end()) ? std::distance(seq.begin(), match) : -1;
+}
+
+template<typename S, typename O>
+bool contains(const S& seq, const O& obj) { return index_of(seq, obj) != -1; }
+
+Command::Command(string word, string arg) :arg(arg) 
+{ 
+    if (int index = index_of(words, word); index != -1) {
+        action = (Action) index;
+    }
+    else throw std::invalid_argument{ "Unrecognized action '" + word + "'" };
+}
+
+
+void GroceryList::load()
+{
+    items.clear();
+
+    std::fstream list_file { source_file, std::fstream::in };
+
+    string line;
+    while (std::getline(list_file, line)) {
+        items.push_back(line);
+    }
+
+    list_file.close();
+}
+
+void GroceryList::save()
+{
+    std::fstream list_file { source_file, 
+        std::fstream::out | std::fstream::trunc };
+
+    items.sort();
+
+    for (string item : items) {
+        list_file << item << "\n";
+    }
+
+    list_file.close();
+}
+
+bool GroceryList::contains(string item) { return ::contains(items, item); }
+
+void GroceryList::set_list(string filename)
+{
+    source_file = filename;
+    load();
+}
+
+void GroceryList::reset() { items.clear(); }
+
+void GroceryList::add(string item)
+{
+    if (contains(item)) {
+        std::cout << "Item '" << item << "' already on list\n";
+    }
+    else {
+        items.push_back(item);
+        save();
+    }
+}
+
+void GroceryList::remove(string item)
+{
+    if (!contains(item)) {
+        std::cout << "Item '" << item << "' not on list; could not remove\n";
+    }
+    else {
+        items.remove(item);
+        save();
+    }
+}
+
+void GroceryList::print()
+{
+    std::cout << "\nGrocery List\n" << "------------\n";
+    for (string item : items) {
+        std::cout << item << "\n";
+    }
+}
+
+void GroceryList::execute(Command& cmd)
+{ 
+    switch (cmd.get_action()) {
+        case Command::Action::LIST:
+            set_list(cmd.get_arg());
+            break;
+
+        case Command::Action::RESET:
+            reset();
+            break;
+
+        case Command::Action::ADD:
+            add(cmd.get_arg());
+            break;
+
+        case Command::Action::REMOVE:
+            remove(cmd.get_arg());
+            break;
+
+        default: // Command::Action::PRINT:
+            print();
+            break;
+    }
 }
 
